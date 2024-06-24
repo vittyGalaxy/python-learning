5f5bc8931d5e474638ebbb60d76ec185e07d5ee3
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-06-24 19:12:17 +0200

    wip: learning operators and I/O

--- main.py
+++ main.py
@@ -1,2 +1,35 @@
 if __name__ == '__main__':
     print("Hello")
+
+    # assegnazione variabile
+    x = "Sera"
+
+    # output
+    print("in questo momento e'" + x)
+
+    # input
+    x = input("Inserisci numero: ")
+
+    # output
+    print(str(x))
+
+    # condition
+    if 5 >= int(x) >= 2:
+        print("Il numero e' maggiore di 1 e minore di 6")
+    else:
+        print("Il numero non e' maggiore di 1 e minore di 6")
+
+    a = True
+    b = False
+
+    # and
+    if a and b:
+        print("Entrambi True")
+    else:
+        print("Uno dei due non e' True")
+
+    # or
+    if a or b:
+        print("Uno dei 2 e' True")
+    else:
+        print("Entrambi False")
