9d97076db13fcf861064618369b4a537fa6d1f92
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-06-25 19:04:44 +0200

    wip: learning boolean

--- main.py
+++ main.py
@@ -1,36 +1,28 @@
 if __name__ == '__main__':
-    a = "ciao"
-    b = " mi piace scrivere"
-    print(a + b)
+    a = True
+    b = False
+    print(a and b)
+    print(b and a)
+    print(a or b)
+    print(b or a)
 
-    c = str(3)
-    print(c + a + b)
+    a = False
+    b = False
+    print(a and b)
+    print(b and a)
+    print(a or b)
+    print(b or a)
 
-    d = a[1:]
-    print(d)
+    a = False
+    b = True
+    print(a and b)
+    print(b and a)
+    print(a or b)
+    print(b or a)
 
-    e = a[2:]
-    print(e)
-
-    f = a[3:]
-    print(f)
-
-    g = a[0:]
-    print(g)
-
-    h = a[:1]
-    print(h)
-
-    i = a[:2]
-    print(i)
-
-    j = a[:3]
-    print(j)
-
-    k = a[:4]
-    print(k)
-
-    l = a[:0]
-    print(l)
-
-    print(h + d)
+    a = True
+    b = True
+    print(a and b)
+    print(b and a)
+    print(a or b)
+    print(b or a)
