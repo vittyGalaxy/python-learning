1b9a4a2a7d32e8c6186b4d70bbebefc9058bc1d2
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-06-25 18:00:51 +0200

    wip: learning types

--- main.py
+++ main.py
@@ -1,38 +1,19 @@
 if __name__ == '__main__':
     a = 3
-    b = 3
-    print(a is b)
+    b = 5.0
+    print(a)
+    print(type(a))
+    print(b)
+    print(type(b))
 
-    a = 2
-    print(a is not b)
-    print(a is b)
-
-    a = "ciao"
-    print("c" in a)
-    print("i" in a)
-    print("a" in a)
-    print("o" in a)
-    print("s" in a)
-
-    a = 4
-    totale = (a * b) + (a * 2)
-    print(totale)
-
-    a += b
+    a **= b
     print(a)
 
-    a -= b
+    a = 3
+    a = float(a)
     print(a)
+    print(type(a))
 
-    a *= b
-    print(a)
-
-    a /= b
-    print(a)
-
-    c = 5
-    print(a > b or a < c)
-    print(b < a < c)
-    print(c > b or a < c)
-    print(b > a and a < c)
-    print(b > a and c < a)
+    b = int(b)
+    print(b)
+    print(type(b))
