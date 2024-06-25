759ca271bcefabf62881aa926fb181f86418b945
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-06-25 18:48:56 +0200

    wip: learning string

--- main.py
+++ main.py
@@ -1,19 +1,36 @@
 if __name__ == '__main__':
-    a = 3
-    b = 5.0
-    print(a)
-    print(type(a))
-    print(b)
-    print(type(b))
+    a = "ciao"
+    b = " mi piace scrivere"
+    print(a + b)
 
-    a **= b
-    print(a)
+    c = str(3)
+    print(c + a + b)
 
-    a = 3
-    a = float(a)
-    print(a)
-    print(type(a))
+    d = a[1:]
+    print(d)
 
-    b = int(b)
-    print(b)
-    print(type(b))
+    e = a[2:]
+    print(e)
+
+    f = a[3:]
+    print(f)
+
+    g = a[0:]
+    print(g)
+
+    h = a[:1]
+    print(h)
+
+    i = a[:2]
+    print(i)
+
+    j = a[:3]
+    print(j)
+
+    k = a[:4]
+    print(k)
+
+    l = a[:0]
+    print(l)
+
+    print(h + d)
