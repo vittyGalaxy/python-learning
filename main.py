d10dca5085bf08ae1e173c8ea2ef2a26d25f5976
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-06-25 19:31:52 +0200

    summary: experiments

--- main.py
+++ main.py
@@ -1,28 +1,28 @@
 if __name__ == '__main__':
-    a = True
-    b = False
-    print(a and b)
-    print(b and a)
-    print(a or b)
-    print(b or a)
+    a = "ciao"
+    b = 3
+    print(str(b) + a)
 
-    a = False
-    b = False
-    print(a and b)
-    print(b and a)
-    print(a or b)
-    print(b or a)
-
-    a = False
-    b = True
-    print(a and b)
-    print(b and a)
-    print(a or b)
-    print(b or a)
+    c = a[:0]
+    d = a[:1]
+    e = a[:2]
+    f = a[:3]
+    g = a[:4]
+    h = a[2:3]
+    print(c)
+    print(d)
+    print(e)
+    print(f)
+    print(g)
+    print(h)
 
     a = True
     b = True
-    print(a and b)
-    print(b and a)
-    print(a or b)
-    print(b or a)
+    c = not(not(a and b) or (b and a) and not b)
+    print(c)
+
+    b = False
+    print(a)
+    print(b)
+    print(int(a))
+    print(int(b))
