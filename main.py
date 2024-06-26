aea891c06369abe0b586427496e4f9f64dd4c6df
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-06-26 19:55:24 +0200

    wip: first exercise with lists in Python

--- main.py
+++ main.py
@@ -1,24 +1,7 @@
 if __name__ == '__main__':
-    lista = [["Hello", 33], ["Word", 3]]
-    print(lista)
-
-    lista1 = ["Hello", "Word"]
-    lista2 = [33, 3]
-    unione = zip(lista1, lista2)
-
-    print(list(unione))
-
-    unione = lista1 + lista2
+    abilita = ["comunicazione", "memoria", "ragionamento", "concentrazione"]
+    voti = [87, 95, 89, 75]
+    abilita.append("agilita mentale")
+    voti.append(80)
+    unione = list(zip(abilita, voti))
     print(unione)
-
-    lista = range(10)
-    print(lista)
-
-    lista = range(10)
-    print(list(lista))
-
-    lista = range(2, 10)
-    print(list(lista))
-
-    lista = range(2, 10, 3)
-    print(list(lista))
