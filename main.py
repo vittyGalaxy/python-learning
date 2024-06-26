3ea2fbe4659586f9ef99e5f4bfab93ca719efe46
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-06-26 21:01:58 +0200

    summary: learning Dictionary

--- main.py
+++ main.py
@@ -1,15 +1,6 @@
 if __name__ == '__main__':
-    t = ()
-    print(type(t))
+    dictionary = {"Hello": 4, "World": 5}
 
-    t = (4)
-    print(type(t))
-
-    t = (4,)
-    print(type(t))
-
-    t = (4, 5, 6)
-    (primo, secondo, terzo) = t
-    print(primo)
-    print(secondo)
-    print(terzo)
+    print(dictionary)
+    print(dictionary["Hello"])
+    print(dictionary["World"])
