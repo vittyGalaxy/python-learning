8b5fea8ad9be7f36412d1f4b1433aab66d9ca8ab
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-06-27 12:11:50 +0200

    wip: learning function

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 if __name__ == '__main__':
-    bar = {"caffe": 1, "cappuccino": 2, "cornetto": 2, "spremuta": 3}
-    print(bar["cornetto"])
-    bar["caffe"] = 2
-    print(bar["cornetto"])
-    prodotti = bar.keys()
-    print(prodotti)
-    bar.update({"cioccolata": 3, "krapfen": 2})
-    print(bar)
-    krapfen = bar.pop("krapfen", 0)
-    print(krapfen)
-    print(bar)
+    def Welcome():
+        print("Hello Word")
+
+    def stampa(string):
+        print(string)
+
+    Welcome()
+    stampa("Hello Word")
