ea8e8995bab9172a157562301d01f46ffbd906d0
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-06-27 19:03:10 +0200

    summary: learning useful functions

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
 if __name__ == '__main__':
-    def ripeti(cosa, numero):
-        return cosa * numero
 
-    testo = ripeti("Hello World ", 3)
-    print(testo)
+    # String
+    # join
+    lista = ["Vittorio", "Angelo"]
+    print(", ".join(lista))
+
+    # replace
+    print("Hello World!".replace("World", "Vittorio"))
+
+    # lower and upper
+    print("Hello World!".lower())
+    print("Hello World!".upper())
+
+    # split
+    print("Hello, World!".split(", "))
+
+    # Numeric
+    # max and min
+    print(min(1, 2, 3, 0, 10))
+    print(max(1, 2, 3, 0, 10))
+    print(max([1, 2, 3, 0, 10]))
+
+    # abs
+    print(abs(-50))
+
+    # sum
+    print(sum([9, 16]))
+
+    # round
+    print(round(34.5678, 2))
