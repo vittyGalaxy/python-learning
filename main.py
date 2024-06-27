4f0d4359765917519fbdf222a4e2cc321e791fbb
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-06-27 17:26:39 +0200

    wip: learning return

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
 if __name__ == '__main__':
-    def stampa():
-        print("Hello Word")
+    def operare(add1, add2):
+        sum = add1 + add2
+        sub = add1 - add2
+        mul = add1 * add2
+        div = add1 / add2
+        return sum, sub, mul, div
 
-    var = stampa()
-    print(var)
+    somma, sottrazione, moltiplicazione, divisione = operare(4, 4)
+
+    print("somma: " + str(somma))
+    print("sottrazione: " + str(sottrazione))
+    print("moltiplicazione: " + str(moltiplicazione))
+    print("divisione: " + str(divisione))
+
+    a = 5
+    def operazione(add1, add2 = a):
+        sum = add1 + add2
+        sub = add1 - add2
+        mul = add1 * add2
+        div = add1 / add2
+        return sum, sub, mul, div
+
+    somma, sottrazione, moltiplicazione, divisione = operazione(4)
+
+    print("somma: " + str(somma))
+    print("sottrazione: " + str(sottrazione))
+    print("moltiplicazione: " + str(moltiplicazione))
+    print("divisione: " + str(divisione))
