46c7fc51c3d358432dea3cc288e6438c78972ced
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-06-27 11:25:06 +0200

    wip: first dictionary exercise

--- main.py
+++ main.py
@@ -1,32 +1,12 @@
 if __name__ == '__main__':
-    dictionary = {"Hello": 4, "World": 5}
-    print(dictionary)
-
-    dictionary.update({"Hello Word": 9})
-    print(dictionary)
-
-    dictionary["Hello"] = 7
-    print(dictionary)
-
-    dictionary["Hello"] = 4
-    print(dictionary)
-
-    print(dictionary.get("Hello", 0))
-    print(dictionary.get("Hi", 0))
-
-    elemento = dictionary.pop("Hello Word", 0)
-    print("elemento: " + str(elemento))
-    print(dictionary)
-
-    elemento = dictionary.pop("Hi", 0)
-    print("elemento: " + str(elemento))
-    print(dictionary)
-
-    chiave = dictionary.keys()
-    print("chiavi: " + str(chiave))
-
-    valore = dictionary.values()
-    print("valori: " + str(valore))
-
-    elementi = dictionary.items()
-    print("elementi: " + str(elementi))
+    bar = {"caffe": 1, "cappuccino": 2, "cornetto": 2, "spremuta": 3}
+    print(bar["cornetto"])
+    bar["caffe"] = 2
+    print(bar["cornetto"])
+    prodotti = bar.keys()
+    print(prodotti)
+    bar.update({"cioccolata": 3, "krapfen": 2})
+    print(bar)
+    krapfen = bar.pop("krapfen", 0)
+    print(krapfen)
+    print(bar)
