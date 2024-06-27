112d9b38a4796952e2976c7fb5812518e2b50c81
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-06-27 11:14:08 +0200

    wip: dictionary exercise

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
 if __name__ == '__main__':
     dictionary = {"Hello": 4, "World": 5}
-
     print(dictionary)
-    print(dictionary["Hello"])
-    print(dictionary["World"])
+
+    dictionary.update({"Hello Word": 9})
+    print(dictionary)
+
+    dictionary["Hello"] = 7
+    print(dictionary)
+
+    dictionary["Hello"] = 4
+    print(dictionary)
+
+    print(dictionary.get("Hello", 0))
+    print(dictionary.get("Hi", 0))
+
+    elemento = dictionary.pop("Hello Word", 0)
+    print("elemento: " + str(elemento))
+    print(dictionary)
+
+    elemento = dictionary.pop("Hi", 0)
+    print("elemento: " + str(elemento))
+    print(dictionary)
+
+    chiave = dictionary.keys()
+    print("chiavi: " + str(chiave))
+
+    valore = dictionary.values()
+    print("valori: " + str(valore))
+
+    elementi = dictionary.items()
+    print("elementi: " + str(elementi))
