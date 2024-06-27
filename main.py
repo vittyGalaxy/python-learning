b3983d4915e79544e5671238d165de6c6a85b9f9
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-06-27 16:17:38 +0200

    wip: learning None

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
 if __name__ == '__main__':
-    def sum(add1, add2):
-        print(add1 + add2)
+    def stampa():
+        print("Hello Word")
 
-
-    def sub(add1, add2=2):
-        print(add1 - add2)
-
-
-    def mul(add1, add2=2):
-        print(add1 * add2)
-
-
-    sum(1, 2)
-    sub(2)
-    mul(3, 3)
+    var = stampa()
+    print(var)
