c096e63b6862ae4f275c45083c94e0e30394e2cd
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-06-28 18:13:02 +0200

    wip: learning recursion (11.50)

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 if __name__ == '__main__':
-    print((lambda x: x ** 2)(2))
-
-    quadrato = lambda x: x ** 2
-    print(quadrato(4))
-
-
-    def funct(f, num):
-        return f(num)
+    def fattoriale(x):
+        if x == 1:
+            return 1
+        else:
+            return x * fattoriale(x - 1)
 
 
-    print(funct(lambda x: x ** 2, 8))
+    print(fattoriale(5))
