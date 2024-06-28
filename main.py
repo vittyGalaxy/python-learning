722c496f8f272a3767ba39b5084d667e0e3a3298
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-06-28 19:23:55 +0200

    wip: learning if nested

--- main.py
+++ main.py
@@ -1,22 +1,8 @@
 if __name__ == '__main__':
-    def convertitore(voto):
-        if voto >= 9.0:
-            return "A"
-
-        elif voto >= 7.0:
-            return "B"
-
-        elif voto >= 6.0:
-            return "C"
-
-        elif voto >= 5.0:
-            return "D"
-
-        else:
-            return "F"
-
-    print(convertitore(9))
-    print(convertitore(7))
-    print(convertitore(6))
-    print(convertitore(5))
-    print(convertitore(2))
+    num = 10
+    if num >= 6:
+        print("Numero maggiore di 6")
+        if num <= 30:
+            print("Il numero e' compreso tra 6 e 30")
+            if num <= 15:
+                print("Il numero e' minore di 15")
