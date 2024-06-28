1a48dcf68ed87baebf32eb1cb37597e9b2418962
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-06-28 19:45:06 +0200

    wip: learning try, except and finally

--- main.py
+++ main.py
@@ -1,8 +1,36 @@
 if __name__ == '__main__':
-    num = 10
-    if num >= 6:
-        print("Numero maggiore di 6")
-        if num <= 30:
-            print("Il numero e' compreso tra 6 e 30")
-            if num <= 15:
-                print("Il numero e' minore di 15")
+    def divide(a, b):
+        try:
+            t = a / b
+            print(t)
+
+        except:
+            print("Attenzione e' successo un casino!")
+
+    divide(3,6)
+    divide(8,0)
+
+    def div(a, b):
+        try:
+            t = a / b
+            print(t)
+
+        except ZeroDivisionError:
+            print("Attenzione non si puo' dividere per zero!")
+
+    div(6,3)
+    div(4,0)
+
+    def divisione(a, b):
+        try:
+            t = a / b
+            print(t)
+
+        except TypeError:
+            print("Attenzione e' successo un casino!")
+
+        finally:
+            print("programma terminato")
+
+    divisione(8, 4)
+    divisione(5, 0)
