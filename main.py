493301620cdf3df0074d84a8a4d434c1e9084d1e
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-06-28 18:17:52 +0200

    summary: learning if (12.50)

--- main.py
+++ main.py
@@ -1,9 +1,41 @@
 if __name__ == '__main__':
-    def sum(x):
-        if x == 1:
-            return 1
-        else:
-            return x + sum(x - 1)
+    def controllo(nome):
+        if nome == "Vittorio":
+            return "Benvenuto Vittorio!"
 
 
-    print(sum(5))
+    print(controllo("Vittorio"))
+    print(controllo("Luca"))
+
+
+    def maggiore(x, y):
+        if x > y:
+            return x
+
+        if x < y:
+            return y
+
+        if x == y:
+            return "I numeri sono uguali"
+
+
+    print(maggiore(6, 4))
+    print(maggiore(3, 4))
+    print(maggiore(4, 4))
+
+
+    def functionAnd(x, y):
+        if (x > 3) and (y > 5):
+            return True
+
+
+    print(functionAnd(4, 6))
+    print(functionAnd(4, 5))
+
+
+    def functionOr(x, y):
+        if (x > 3) or (y > 5):
+            return True
+
+
+    print(functionOr(4, 5))
