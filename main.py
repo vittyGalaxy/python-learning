a19a3c6930fbfe85b99490f54aa341359f95a827
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-06-28 20:38:44 +0200

    summary: created "heads or tails" game

--- main.py
+++ main.py
@@ -1,30 +1,23 @@
 if __name__ == '__main__':
-    def super_potenza(x, y):
-        if (x ** y) > 4000:
-            return True
+    import random
+    soldi = 100
+
+    def testa_o_croce(puntata, testa_croce):
+        lancio = random.randint(0, 1)
+
+        if lancio == 0:
+            if testa_croce == "Testa":
+                print("Testa, hai vinto! Il tuo saldo e;: " + str(soldi + puntata))
+
+            else:
+                print("Testa, hai perso! Il tuo saldo e': " + str(soldi - puntata))
 
         else:
-            return False
+            if testa_croce == "Croce":
+                print("Croce, hai vinto! Il tuo saldo e': " + str(soldi + puntata))
 
+            else:
+                print("Croce, hai perso! Il tuo saldo e': " + str(soldi - puntata))
 
-    def diviso_10(x):
-        if (x % 10) == 0:
-            return True
-
-        else:
-            return False
-
-    def in_mezzo(num, alto, basso):
-        if (num > basso) and (num < alto):
-            return True
-
-        else:
-            return False
-
-    print(super_potenza(30, 5))
-    print(diviso_10(20))
-    print(in_mezzo(22, 33, 11))
-
-    print(super_potenza(1, 2))
-    print(diviso_10(42))
-    print(in_mezzo(2000, 1, 10))
+    testa_o_croce(10, "Testa")
+    testa_o_croce(10, "Croce")
