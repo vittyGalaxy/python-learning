252713c1c0f5fff374f22faed7c7109a34d172fe
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-07-01 18:30:33 +0200

    wip: data arrangement

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 if __name__ == '__main__':
 
-    lettere = ["a", "b", "c", "d", "e"]
-    punti = ["1", "3", "2", "5", "2"]
-    a = zip(lettere, punti)
-    print(a)
+    libri = "Tentazioni: Aldo Bruno: 1990, Attacco cartico: Marino Carra: 1988, Santo subito!: Gio Batta:2001, Sempre tuo: Stefano Cosmi: 1997"
+    lista_libri = libri.split(",")
+    print(lista_libri)
 
-    a = list(zip(lettere, punti))
-    print(a)
+    lista_libri_dettagli = []
 
-    dictionary = {key: value for key, value in a}
-    print(dictionary)
+    for libro in lista_libri:
+        lista_libri_dettagli.append(libro.split(":"))
+
+    print(lista_libri_dettagli)
+
+    titolo = []
+    autore = []
+    data = []
+
+    for libro in lista_libri_dettagli:
+        titolo.append(libro[0])
+        autore.append(libro[1])
+        data.append(libro[2])
+
+    print(titolo, autore, data)
+
+    for i in range(0, len(lista_libri_dettagli)):
+        print("Il libro\"{}\" e' stato scrittto da {} nel {} .\n".format(titolo[i], autore[i], data[i]))
