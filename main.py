4c0694b60d76845d850873237bd0d87f5ca55866
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-07-01 12:42:58 +0200

    summary: learning nested loop

--- main.py
+++ main.py
@@ -1,40 +1,10 @@
 if __name__ == '__main__':
-    import random
 
-    soldi = 100
+    lista = [[12, 34, 34], [2, 5, 67], [2, 4]]
+    total = 0
 
+    for i in lista:
+        for e in i:
+            total += e
 
-    def testa_o_croce(puntata, testa_croce):
-        global soldi
-        lancio = random.randint(0, 1)
-        if lancio == 0:
-            if testa_croce == "Testa":
-                soldi += puntata
-                print("Testa, hai vinto! Il tuo saldo e: " + str(soldi) + ".")
-
-            else:
-                soldi -= puntata
-                print("Testa, hai perso! Il tuo saldo e': " + str(soldi) + ".")
-
-        else:
-            if testa_croce == "Croce":
-                soldi += puntata
-                print("Croce, hai vinto! Il tuo saldo e': " + str(soldi) + ".")
-
-            else:
-                soldi -= puntata
-                print("Croce, hai perso! Il tuo saldo e': " + str(soldi) + ".")
-
-
-    num = input("Scrivi 0 per smettere di giocare oppure un qualsiasi numero per continuare: ")
-    while (num != 0) and (soldi >= 0):
-        punta = input("Inserisci quanto punti: ")
-        t_c = input("Inserisci 'Testa' o 'Croce': ")
-
-        testa_o_croce(int(punta), str(t_c))
-
-        num = input("Scrivi 0 per smettere di giocare oppure un qualsiasi numero per continuare: ")
-        if soldi <= 0:
-            print("Hai finito i soldi")
-
-    print("Grazie per aver giocato!")
+    print(total)
