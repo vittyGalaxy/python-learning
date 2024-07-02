8cfdd575a07d3ecb87e5eef261ec9d895bdd2dd1
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-07-02 11:47:55 +0200

    wip: hangman game update

--- main.py
+++ main.py
@@ -4,23 +4,22 @@ if __name__ == '__main__':
     liste_parole = ["hello", "world"]
     scelta_parole = random.choice(liste_parole)
 
-    indovina = input('indovina la lettera: ')
-
-    for l in scelta_parole:
-        if l == indovina:
-            print('Lettera azzeccata')
-        else:
-            print('Lettera non azzeccata')
+    game_over = False
 
     campo_gioco = []
     for i in scelta_parole:
         campo_gioco += '_'
 
-    for posizione in range(len(scelta_parole)):
-        lettera = scelta_parole[posizione]
-        if lettera == indovina:
-            campo_gioco[posizione] = lettera
+    while not game_over:
+        indovina = input('indovina la lettera: ')
+        for posizione in range(len(scelta_parole)):
+            lettera = scelta_parole[posizione]
+            if lettera == indovina:
+                campo_gioco[posizione] = lettera
+        print(f"{' '.join(campo_gioco)}")
 
-    print(campo_gioco)
+        if not '_' in campo_gioco:
+            game_over = True
+            print('Complimenti hai vinto!')
 
     print(scelta_parole)
