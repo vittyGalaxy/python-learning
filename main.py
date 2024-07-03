61c041f01ed33970191d818ea2bdce4258dad69b
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-07-03 19:52:37 +0200

    summary: exercise 11

--- main.py
+++ main.py
@@ -1,24 +1,10 @@
-def binary_search(search_list, search_item):
-    search_list.sort()
-
-    if len(search_list) == 0:
-        return False
-
-    midpoint = len(search_list) // 2
-
-    if search_list[midpoint] == search_item:
-        return True
-    elif search_item < search_list[midpoint]:
-        return binary_search(search_list[:midpoint], search_item)
-    else:
-        return binary_search(search_list[midpoint + 1:], search_item)
+def converter(d, h, m):
+    s = (m * 60) + (h * 3600) + (d * 86400)
+    return s
 
 if __name__ == '__main__':
 
-    lst = [12, 64, 23, 3, 57, 19, 1, 17, 51, 62]
-
-    print("12 e' nella lista: ", binary_search(lst, 12))
-    print("5 e' nella lista: ", binary_search(lst, 5))
-    print("64 e' nella lista: ", binary_search(lst, 64))
-    print("3 e' nella lista: ", binary_search(lst, 3))
-    print("7 e' nella lista: ", binary_search(lst, 7))
+    days = input("Inserisci i giorni: ")
+    hours = input("Inserisci le ore: ")
+    minutes = input("Inserisci i minuti: ")
+    print("I secondi sono: ", converter(days, hours, minutes))
