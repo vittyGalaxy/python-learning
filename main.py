b6320f46f37597aaf10c6a87189a637a0406a62f
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-07-03 18:07:39 +0200

    wip: learning merge sort

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 if __name__ == '__main__':
-    def dizionario(stringa):
-        dictionary = {}
-        l = 0
-        for i in stringa:
-            if i in dictionary:
-                dictionary[i] += 1
-            else:
-                dictionary[i] = 1
+    def merge_sort(lst):
 
-        return dictionary
+        if len(lst) <= 1:
+            return lst
 
-    print(dizionario("hello"))
+        else:
+            punto_medio = len(lst) // 2
+
+            sinistra = merge_sort(lst[:punto_medio])
+            destra = merge_sort(lst[punto_medio:])
+
+            nuova_lista = []
+            while len(sinistra) and len(destra) > 0:
+
+                if sinistra[0] < destra[0]:
+                    nuova_lista.append(sinistra[0])
+                    del sinistra[0]
+
+                else:
+                    nuova_lista.append(destra[0])
+                    del destra[0]
+
+            nuova_lista.extend(sinistra)
+            nuova_lista.extend(destra)
+
+            return nuova_lista
+
+    print(merge_sort([2, 5, 3, 8, 6, 9, 1, 4, 7]))
