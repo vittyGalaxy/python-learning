a765169ce7190be5c1e83c9f9114c444dca8e50b
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-07-04 12:31:34 +0200

    summary: exercise 14

--- main.py
+++ main.py
@@ -1,29 +1,22 @@
-def shape_area(choice):
-    choice.lower()
-    if choice == "square":
-        b = input("Inserisci base: ")
-        area = int(b) * 4
+def count_duplications(a, b):
+    value = 0
+    for i in a:
+        if i == b:
+            value += 1
 
-    if choice == "rectangle":
-        b = input("Inserisci la base: ")
-        h = input("Inserisci l'altezza: ")
-        area = int(b) * int(h)
-
-    if choice == "triangle":
-        b = input("Inserisci la base: ")
-        h = input("Inserisci l'altezza: ")
-        area = (float(b) * float(h)) / 2
-
-    if choice == "circle":
-        r = input("Inserisci il raggio: ")
-        area = 3.14 * (int(r) ** 2)
-
-    return area
+    return value
 
 
 if __name__ == '__main__':
 
-    print(shape_area("square"))
-    print(shape_area("triangle"))
-    print(shape_area("circle"))
-    print(shape_area("rectangle"))
+    c = [2, 3, 4, 4, 4, 5, 6, 7, 7, 8, 8, 8, 9, 9, 10]
+    print(count_duplications(c, 2))
+    print(count_duplications(c, 3))
+    print(count_duplications(c, 4))
+    print(count_duplications(c, 5))
+    print(count_duplications(c, 6))
+    print(count_duplications(c, 7))
+    print(count_duplications(c, 8))
+    print(count_duplications(c, 9))
+    print(count_duplications(c, 10))
+    print(count_duplications(c, 1))
