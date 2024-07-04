c072cc3c3f4c88c6a932ac8680568f273fe0c8bf
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-07-04 11:38:26 +0200

    wip: exercise 12

--- main.py
+++ main.py
@@ -1,34 +1,9 @@
+def execute_factorial(a):
+    if a == 1:
+        return a
+    else:
+        return a * execute_factorial(a - 1)
+
 if __name__ == '__main__':
 
-    f = open("file.txt", "r")
-    content = f.read()
-
-    print(content)
-    f.close()
-
-    f = open("file.txt", "w")
-
-    f.write("Ciao")
-
-    f.close()
-
-    f = open("file.txt", "r")
-    content = f.read()
-
-    print(content)
-    f.close()
-
-    with open("file.txt") as name_file:
-        content = name_file.read()
-
-        print(content)
-
-
-    with open("file.txt", "a") as name_file:
-        name_file.write(" Bene")
-
-
-    with open("file.txt") as name_file:
-        content = name_file.read()
-
-        print(content)
+    print(execute_factorial(5))
