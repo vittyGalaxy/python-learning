0889dbbb349c50a786f43f6dd5f2bedf7389cf8d
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-07-04 12:13:40 +0200

    wip: exercise 13

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
-def execute_factorial(a):
-    if a == 1:
-        return a
-    else:
-        return a * execute_factorial(a - 1)
+def shape_area(choice):
+    choice.lower()
+    if choice == "square":
+        b = input("Inserisci base: ")
+        area = int(b) * 4
+
+    if choice == "rectangle":
+        b = input("Inserisci la base: ")
+        h = input("Inserisci l'altezza: ")
+        area = int(b) * int(h)
+
+    if choice == "triangle":
+        b = input("Inserisci la base: ")
+        h = input("Inserisci l'altezza: ")
+        area = (float(b) * float(h)) / 2
+
+    if choice == "circle":
+        r = input("Inserisci il raggio: ")
+        area = 3.14 * (int(r) ** 2)
+
+    return area
+
 
 if __name__ == '__main__':
 
-    print(execute_factorial(5))
+    print(shape_area("square"))
+    print(shape_area("triangle"))
+    print(shape_area("circle"))
+    print(shape_area("rectangle"))
