de15df857322eb155f0e93f04574f9206f00bfd6
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-07-04 18:47:38 +0200

    wip: exercise practice classes

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-class Dog:
-    dog_age_difference = 7
+class Circle:
+    pi = 3.14
 
-    def explanation_time(self):
-        print("I cani hanno {} anni per ogni anno dell'essere umano".format(self.dog_age_difference))
-
+    def area(self, radius):
+        return Circle.pi * (radius ** 2)
 
 
 
 if __name__ == '__main__':
-    a = Dog()
-    a.explanation_time()
+    circle = Circle()
 
+    fountain = circle.area(30)
+
+    print(fountain)
