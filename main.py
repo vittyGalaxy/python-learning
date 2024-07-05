53bbde463e212015a7d28999f41f831b4d45a329
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-07-05 11:27:10 +0200

    wip: new method

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
-class Circle:
-    pi = 3.14
+class Name:
+    def __init__(self, name):
+        self.name = name
 
-    def __init__(self, diameter):
-        print("Crea un cerchio con diametro {d}".format(d = diameter))
-        self.radius = diameter / 2
-
-    def circumference(self):
-        return 2 * self.pi * self.radius
+    def __repr__(self):
+        return "Il mio nome e' " + self.name
 
 if __name__ == '__main__':
-    round_table = Circle(50)
-    print(round_table.circumference())
+    a = Name("Vittorio")
+    print(a)
