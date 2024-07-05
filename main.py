7a218cd911fe25ea48dc2d2ca4a008da07b9179c
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-07-05 12:39:59 +0200

    summary: experiment inheritance class

--- main.py
+++ main.py
@@ -1,24 +1,14 @@
-class message:
+class Salad:
     admin = False
-    def __init__(self, sender, object, text):
-        self.sender = sender
-        self.object = object
-        self.text = text
+    def __init__(self, tomatoes, onion, beans):
+        self.tomatoes = tomatoes
+        self.onion = onion
+        self.beans = beans
 
 
-class User:
-    def __init__(self, name_user):
-        self.name_user = name_user
-
-    def edit_message(self, message, new_text):
-        if message.sender == self.name_user:
-            message.text = new_text
-
-class Admin(User):
-    def edit_message(self, message, new_text):
-        message.text = new_text
-
-
-
+class SaladTop(Salad):
+    def __init__(self, tomatoes, onion, beans):
+        super().__init__(tomatoes, onion, beans)
+        self.garlic = True
 
 if __name__ == '__main__':
