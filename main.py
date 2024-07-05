2ab72215a8a9d6c31c3525d48e1c953a74cbacc5
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-07-05 19:06:42 +0200

    wip: exercise 17

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 if __name__ == '__main__':
 
-    string = input("Inserisci cosa vuoi mettere nel file: ")
-    with open("file.txt","w") as test:
-        content = test.write(string)
+    a = input("Inserisci cosa vuoi mettere nel file: ")
+    with open("file_copia.txt", "w") as b:
+        b = b.write(a)
 
-    with open("file.txt") as test:
-        print(test.read())
+    with open("file_copia.txt") as content:
+        content = content.read()
+
+
+    with open("file_copia.txt", "w") as test:
+        test.write(content)
+
+    with open("file_copia.txt") as a:
+        print(a.read())
