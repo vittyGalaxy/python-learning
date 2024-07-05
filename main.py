ec880d73696bffd8d3a690175b13f27832387bf7
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-07-05 17:12:03 +0200

    wip: learning polymorphism in the classes

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-if __name__ == '__main__':
-    a = [1, 2]
-    b = "Ciao"
+class Color:
+    def __init__(self, red, green, blu):
+        self.red = red
+        self.green = green
+        self.blu = blu
 
-    print(len(a))
-    print(len(b))
+    def __repr__(self):
+        return "Colore in RGB = {red}, {green}, {blu}".format(red = self.red, green = self.green, blu = self.blu)
+
+    def __add__(self, other):
+        new_red = min(self.red + other.red, 255)
+        new_green = min(self.green + other.green, 255)
+        new_blu = min(self.blu + other.blu, 255)
+        return Color(new_red, new_green, new_blu)
+
+
+if __name__ == '__main__':
+    red = Color(255, 0,0)
+    green = Color(0, 255,0)
+
+    yellow = red + green
+    print(yellow)
