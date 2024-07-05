1327535603abc19f8100501728ab09498552e5ed
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-07-05 19:59:17 +0200

    summary: exercise 22

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-def word_length(a):
-    result = []
-
+def max_number(a):
+    m = a[0]
     for i in a:
-        content = len(i)
-        result.append(content)
+        if i > m:
+            m = i
 
-    return result
+    return m
 
 if __name__ == '__main__':
 
-    b = ["Hello", "World!"]
-    print(word_length(b))
+    b = [1, 5, 2, 22, 99, 100, 2, 4, 8]
+    print(max_number(b))
