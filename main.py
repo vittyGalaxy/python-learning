2048e32c844f6dfa7061eea452bcd6d87fd14702
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-07-05 12:35:22 +0200

    wip: experiment inheritance class

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
-class User:
+class message:
     admin = False
+    def __init__(self, sender, object, text):
+        self.sender = sender
+        self.object = object
+        self.text = text
+
+
+class User:
     def __init__(self, name_user):
         self.name_user = name_user
 
+    def edit_message(self, message, new_text):
+        if message.sender == self.name_user:
+            message.text = new_text
 
 class Admin(User):
-    admin = True
+    def edit_message(self, message, new_text):
+        message.text = new_text
+
+
 
 
 if __name__ == '__main__':
-    print(issubclass(Admin, User))
