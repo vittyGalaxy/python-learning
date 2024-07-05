0eafe922f188996107d40bfbd5c43f2221b0b479
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-07-05 18:36:24 +0200

    wip: exercise file

--- main.py
+++ main.py
@@ -1,18 +1,5 @@
-import csv
-
 if __name__ == '__main__':
-    elenco_nomi = [{"Nome": "Vittorio", "Cognome": "Tiozzo", "Email": "vittorio@gmail.com"}]
-    int_colonne = ["Nome", "Cognome", "Email"]
 
-    # with open("file.csv", "w") as nomi_elenco:
-    #     scrittura = csv.DictWriter(nomi_elenco, fieldnames = int_colonne)
-    #
-    #     scrittura.writeheader()
-    #
-    #     for elemento in elenco_nomi:
-    #         scrittura.writerow(elemento)
-
-    with open("file.csv") as elenco:
-        content = elenco.read()
-
-        print(content)
+    with open("file.txt") as test:
+        for i in test:
+            print(i)
