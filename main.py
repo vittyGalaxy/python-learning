a47bf98bdc7415d1318888fba0e605fadca17f0c
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-07-05 16:56:09 +0200

    wip: learning polymorphism

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-class Salad:
-    admin = False
-    def __init__(self, tomatoes, onion, beans):
-        self.tomatoes = tomatoes
-        self.onion = onion
-        self.beans = beans
-
-
-class SaladTop(Salad):
-    def __init__(self, tomatoes, onion, beans):
-        super().__init__(tomatoes, onion, beans)
-        self.garlic = True
-
 if __name__ == '__main__':
+    a = [1, 2]
+    b = "Ciao"
+
+    print(len(a))
+    print(len(b))
