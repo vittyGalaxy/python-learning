c54c32500ce19b6e1ef7a5dc6b92d5d265283b7e
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-07-05 19:39:02 +0200

    wip: exercise 19

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-def sum(a):
-    s = 0
-    for i in a:
-        s += i
-    return s
+def search(a, c):
+    risult = []
+
+    for word in a:
+        if word[0] == c:
+            risult.append(word)
+
+    return risult
 
 if __name__ == '__main__':
 
-    b = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-    print(sum(b))
+    b = ["Hello", "World!"]
+    print(search(b, "H"))
+    print(search(b, "W"))
