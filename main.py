2cd4f93a89a5ec65fb6144a753bbf8757920b755
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-07-08 18:05:37 +0200

    wip: exercise 26

--- main.py
+++ main.py
@@ -1,45 +1,14 @@
-class Rational:
-    def __init__(self, number1, number2):
-        self.number1 = number1
-        self.number2 = number2
+class Product:
+    def __init__(self, name, price, amount):
+        self.name = name
+        self.price = price
+        self.amount = amount
 
-    def sum(self):
-        return self.number1 + self.number2
-
-    def subtraction(self):
-        return self.number1 - self.number2
-
-    def multiplication(self):
-        return self.number1 * self.number2
-
-    def division(self):
-        r1 = self.number1
-        r2 = self.number2
-        for i in range(2, self.number1 + 1):
-            if ((self.number1 % i) == 0) and ((self.number2 % i) == 0):
-                r1 = self.number1 / i
-                r2 = self.number2 / i
-                i = 2
-        print("Razionalizzando e': " + str(r1) + "/" + str(r2))
-        return self.number1 / self.number2
+    def print_product(self):
+        print("Il nome del prodotto e' " + self.name + " ce ne sono " + str(self.amount) + " e costa " + str(self.price) + " euro")
 
 
 if __name__ == '__main__':
 
-    r = Rational(5,10)
-    print(r.sum())
-    print(r.subtraction())
-    print(r.multiplication())
-    print(r.division())
-
-    r = Rational(20,40)
-    print(r.sum())
-    print(r.subtraction())
-    print(r.multiplication())
-    print(r.division())
-
-    r = Rational(30,46)
-    print(r.sum())
-    print(r.subtraction())
-    print(r.multiplication())
-    print(r.division())
+    p = Product("Cereali", 10, 3)
+    p.print_product()
