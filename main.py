3a0a7eed27aea8e93629b1e328dad196f6697f7a
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-07-08 17:26:45 +0200

    wip: exercise 25

--- main.py
+++ main.py
@@ -1,15 +1,45 @@
-class Counter:
-    def __init__(self, counter):
-        print("Contero' +1 alla volta")
-        self.counter = counter
+class Rational:
+    def __init__(self, number1, number2):
+        self.number1 = number1
+        self.number2 = number2
 
-    def increase(self):
-        self.counter += 1
-        return self.counter
+    def sum(self):
+        return self.number1 + self.number2
+
+    def subtraction(self):
+        return self.number1 - self.number2
+
+    def multiplication(self):
+        return self.number1 * self.number2
+
+    def division(self):
+        r1 = self.number1
+        r2 = self.number2
+        for i in range(2, self.number1 + 1):
+            if ((self.number1 % i) == 0) and ((self.number2 % i) == 0):
+                r1 = self.number1 / i
+                r2 = self.number2 / i
+                i = 2
+        print("Razionalizzando e': " + str(r1) + "/" + str(r2))
+        return self.number1 / self.number2
+
 
 if __name__ == '__main__':
 
-    c = Counter(0)
-    print(c.increase())
-    print(c.increase())
-    print(c.increase())
+    r = Rational(5,10)
+    print(r.sum())
+    print(r.subtraction())
+    print(r.multiplication())
+    print(r.division())
+
+    r = Rational(20,40)
+    print(r.sum())
+    print(r.subtraction())
+    print(r.multiplication())
+    print(r.division())
+
+    r = Rational(30,46)
+    print(r.sum())
+    print(r.subtraction())
+    print(r.multiplication())
+    print(r.division())
