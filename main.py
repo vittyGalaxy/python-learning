a619fea555eff811821c5276c00800f4ca0815d3
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-07-08 11:26:06 +0200

    wip: fixed exercise 23

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 class Persona:
-    def __init__(self, name, age, sesso):
-        print("Ciao, mi chiamo " + name + ", sono " + sesso + " e ho " + str(age) + " anni")
+    def __init__(self, name, age, sex):
+        self.name = name
+        self.age = age
+        self.sex = sex
+
+    def presentation(self):
+        print(f"Ciao, mi chiamo {self.name}, sono {self.sex} e ho {self.age} anni")
 
 if __name__ == '__main__':
 
-    Persona("Vittorio", 17, "Maschio")
+    persona = Persona("Vittorio", 17, "Maschio")
+    persona.presentation()
