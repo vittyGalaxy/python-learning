ce945de2fa43aeca374b3b947b58d2f5e50687c5
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-07-08 12:28:21 +0200

    summary: exercise 24

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-class Persona:
-    def __init__(self, name, age, sex):
-        self.name = name
-        self.age = age
-        self.sex = sex
+class Counter:
+    def __init__(self, counter):
+        print("Contero' +1 alla volta")
+        self.counter = counter
 
-    def presentation(self):
-        print(f"Ciao, mi chiamo {self.name}, sono {self.sex} e ho {self.age} anni")
+    def increase(self):
+        self.counter += 1
+        return self.counter
 
 if __name__ == '__main__':
 
-    persona = Persona("Vittorio", 17, "Maschio")
-    persona.presentation()
+    c = Counter(0)
+    print(c.increase())
+    print(c.increase())
+    print(c.increase())
