9557771968b3ac9b68aafc12d25a2e925e1b4dfe
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-07-09 13:01:22 +0200

    summary: wip exercise 32

--- main.py
+++ main.py
@@ -8,15 +8,14 @@ class Animals:
 
     def recognition(self):
         if self.animals == "gatto" or self.animals == "cane":
-            print("Questo e' un ")
-            return self.animals
-        else:
-            return print("Non riconosco questo animale")
+            return print("Questo e' un " + Animals.getAnimals(self))
+
+        return print("Non riconosco questo animale")
+
 
 if __name__ == '__main__':
     a = Animals("gatto")
     print(a.recognition())
-    print(a.getAnimals())
     a = Animals("cane")
     print(a.recognition())
-    print(a.getAnimals())
+    a = Animals("wwwww")
