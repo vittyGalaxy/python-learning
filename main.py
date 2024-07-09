cc5e33140550d93750091b7db82f5712d6afbdb2
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-07-09 12:36:00 +0200

    wip: exercise 32

--- main.py
+++ main.py
@@ -1,37 +1,22 @@
-import math
-
-class Triangle:
-    def __init__(self, a, b, c):
-        if not (a + b > c) and (a + c > b) and (b + c > a):
-            raise ValueError("I lati non possono formare un triangolo")
-
-        self.a = a
-        self.b = b
-        self.c = c
+class Animals:
+    def __init__(self, animals):
+        self.animals = animals
 
 
-    def getA(self):
-        return self.a
+    def getAnimals(self):
+        return self.animals
 
-    def getB(self):
-        return self.b
-
-    def getC(self):
-        return self.c
-
-    def area(self):
-        s = self.perimeter() / 2
-        return math.sqrt(s * (s - self.a) * (s - self.b) * (s - self.c))
-
-    def perimeter(self):
-        return self.a + self.b + self.c
-
-    def __str__(self):
-        return "Il perimetro e' " + str(Triangle.perimeter(self)) + " e l'area e' " + str(Triangle.area(self))
+    def recognition(self):
+        if self.animals == "gatto" or self.animals == "cane":
+            print("Questo e' un ")
+            return self.animals
+        else:
+            return print("Non riconosco questo animale")
 
 if __name__ == '__main__':
-    t = Triangle(6.0, 8.0, 10.0)
-    print(Triangle(6.0, 8.0, 10.0))
-    print(t.getA())
-    print(t.getB())
-    print(t.getC())
+    a = Animals("gatto")
+    print(a.recognition())
+    print(a.getAnimals())
+    a = Animals("cane")
+    print(a.recognition())
+    print(a.getAnimals())
