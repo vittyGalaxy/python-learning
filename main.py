d81622b44ec183d7f016f20c7766ae08eb9425c7
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-07-09 11:46:12 +0200

    wip: exercise 31

--- main.py
+++ main.py
@@ -1,20 +1,37 @@
-class Square:
-    def __init__(self, base):
-        self.base = base
+import math
 
-    def getBase(self):
-        return self.base
+class Triangle:
+    def __init__(self, a, b, c):
+        if not (a + b > c) and (a + c > b) and (b + c > a):
+            raise ValueError("I lati non possono formare un triangolo")
+
+        self.a = a
+        self.b = b
+        self.c = c
+
+
+    def getA(self):
+        return self.a
+
+    def getB(self):
+        return self.b
+
+    def getC(self):
+        return self.c
 
     def area(self):
-        return self.base * self.getBase()
+        s = self.perimeter() / 2
+        return math.sqrt(s * (s - self.a) * (s - self.b) * (s - self.c))
 
     def perimeter(self):
-        return self.base * 4
+        return self.a + self.b + self.c
 
     def __str__(self):
-        return "Il perimetro e' " + str(Square.perimeter(self)) + " e l'area e' " + str(Square.area(self))
+        return "Il perimetro e' " + str(Triangle.perimeter(self)) + " e l'area e' " + str(Triangle.area(self))
 
 if __name__ == '__main__':
-    s = Square(5.0)
-    print(Square(5.0))
-    print(s.getBase())
+    t = Triangle(6.0, 8.0, 10.0)
+    print(Triangle(6.0, 8.0, 10.0))
+    print(t.getA())
+    print(t.getB())
+    print(t.getC())
