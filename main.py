c29aad50555c7d78a6ce277f6192fdad1ef4be9a
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-07-10 10:59:47 +0200

    wip: exercise 33

--- main.py
+++ main.py
@@ -1,21 +1,48 @@
-class Animals:
-    def __init__(self, animals):
-        self.animals = animals
+class Vehicle:
+    def __init__(self, brand, model, year):
+        self.brand = brand
+        self.model = model
+        self.year = year
 
+    def accelerate(self):
+        print("Sto accellerando")
 
-    def getAnimals(self):
-        return self.animals
+    def brake(self):
+        print("Sto frenando")
 
-    def recognition(self):
-        if self.animals == "gatto" or self.animals == "cane":
-            return print("Questo e' un " + Animals.getAnimals(self))
+    def getBrand(self):
+        return self.brand
 
-        return print("Non riconosco questo animale")
+    def getModel(self):
+        return self.model
+
+    def getYear(self):
+        return self.year
+
+    def __str__(self):
+        return f"Brand: {self.brand}, Model: {self.model}, Year: {self.year}"
+
+class Car(Vehicle):
+    def __init__(self, brand, model, year, color):
+        super().__init__(brand, model, year)
+        self.color = color
+
+    def change_colour(self, new_color):
+        self.color = new_color
+
+    def getColor(self):
+        return self.color
+
+    def __str__(self):
+        return super().__str__() + f", Color: {self.color}"
 
 
 if __name__ == '__main__':
-    a = Animals("gatto")
-    print(a.recognition())
-    a = Animals("cane")
-    print(a.recognition())
-    a = Animals("wwwww")
+    v = Vehicle("Jeep", "Compass", 5)
+    print(v.getBrand())
+    print(v.getModel())
+    print(v.getYear())
+    print(Vehicle("Jeep", "Compass", 5))
+    c = Car("Jeep", "Compass", 5, "Yellow")
+    print(c.getColor())
+    print(Car("Jeep", "Compass", 5, "Yellow"))
