38286b1bc0ab6c273e0b98daaed5724d781634f8
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-07-10 12:50:11 +0200

    summary: exercise 36

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 class Student:
-    def __init__(self, name, surname):
+    def __init__(self, name, surname, vote):
         self.name = name
         self.surname = surname
+        self.vote = vote
 
     def getName(self):
         return self.name
@@ -9,13 +10,18 @@ class Student:
     def getSurname(self):
         return self.surname
 
-    def __str__(self):
-        return f"Ciao! Mi chiamo {self.name} {self.surname}!"
+    def getVote(self):
+        return self.vote
+
+    def calculate_average(self):
+        average = sum(self.vote) / len(self.getVote())
+        return average
 
 
 
 if __name__ == '__main__':
-    s = Student("Vittorio", "Tiozzo")
+    s = Student("Vittorio", "Tiozzo", [6, 7, 8, 9, 10])
     print(s.getName())
     print(s.getSurname())
-    print(Student("Vittorio", "Tiozzo"))
+    print(s.getVote())
+    print(s.calculate_average())
