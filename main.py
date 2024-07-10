859464d965c441f809e605f87fd74f77b84d4750
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-07-10 12:42:51 +0200

    wip: exercise 35

--- main.py
+++ main.py
@@ -1,39 +1,21 @@
-import math
+class Student:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
 
-class Form:
-    def area(self):
-        pass
+    def getName(self):
+        return self.name
 
+    def getSurname(self):
+        return self.surname
 
-
-class Square(Form):
-    def __init__(self, base):
-        super().__init__()
-        self.base = base
-
-    def getBase(self):
-        return self.base
-
-    def area(self):
-        return self.base ** 2
-
-class Circle(Form):
-    def __init__(self, radius):
-        super().__init__()
-        self.radius = radius
-
-    def getRadius(self):
-        return self.radius
-
-    def area(self):
-        return math.pi * self.radius ** 2
+    def __str__(self):
+        return f"Ciao! Mi chiamo {self.name} {self.surname}!"
 
 
 
 if __name__ == '__main__':
-    q = Square(5)
-    print(q.getBase())
-    print(q.area())
-    c = Circle(5)
-    print(c.getRadius())
-    print(c.area())
+    s = Student("Vittorio", "Tiozzo")
+    print(s.getName())
+    print(s.getSurname())
+    print(Student("Vittorio", "Tiozzo"))
