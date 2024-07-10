0b971b5222753a09c64931f4b2651fc56c6efe0f
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-07-10 12:13:10 +0200

    wip: exercise 34

--- main.py
+++ main.py
@@ -1,48 +1,39 @@
-class Vehicle:
-    def __init__(self, brand, model, year):
-        self.brand = brand
-        self.model = model
-        self.year = year
+import math
 
-    def accelerate(self):
-        print("Sto accellerando")
+class Form:
+    def area(self):
+        pass
 
-    def brake(self):
-        print("Sto frenando")
 
-    def getBrand(self):
-        return self.brand
 
-    def getModel(self):
-        return self.model
+class Square(Form):
+    def __init__(self, base):
+        super().__init__()
+        self.base = base
 
-    def getYear(self):
-        return self.year
+    def getBase(self):
+        return self.base
 
-    def __str__(self):
-        return f"Brand: {self.brand}, Model: {self.model}, Year: {self.year}"
+    def area(self):
+        return self.base ** 2
 
-class Car(Vehicle):
-    def __init__(self, brand, model, year, color):
-        super().__init__(brand, model, year)
-        self.color = color
+class Circle(Form):
+    def __init__(self, radius):
+        super().__init__()
+        self.radius = radius
 
-    def change_colour(self, new_color):
-        self.color = new_color
+    def getRadius(self):
+        return self.radius
 
-    def getColor(self):
-        return self.color
+    def area(self):
+        return math.pi * self.radius ** 2
 
-    def __str__(self):
-        return super().__str__() + f", Color: {self.color}"
 
 
 if __name__ == '__main__':
-    v = Vehicle("Jeep", "Compass", 5)
-    print(v.getBrand())
-    print(v.getModel())
-    print(v.getYear())
-    print(Vehicle("Jeep", "Compass", 5))
-    c = Car("Jeep", "Compass", 5, "Yellow")
-    print(c.getColor())
-    print(Car("Jeep", "Compass", 5, "Yellow"))
+    q = Square(5)
+    print(q.getBase())
+    print(q.area())
+    c = Circle(5)
+    print(c.getRadius())
+    print(c.area())
