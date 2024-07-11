a5cf7b37d4f32f2243bacacaa78a35db5ed44955
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-07-11 10:56:51 +0200

    wip: exercise 41

--- main.py
+++ main.py
@@ -1,34 +1,47 @@
-class Book:
-    def __init__(self, title, author):
-        self.title = title
-        self.author = author
+class BankAccount:
+    def __init__(self, holder, balance = 0):
+        self.holder = holder
+        self.balance = balance
+    def getHolder(self):
+        return self.holder
 
-    def getTitle(self):
-        return self.title
+    def getBalance(self):
+        return self.balance
 
-    def getAuthor(self):
-        return self.author
+    def deposit(self, amount):
+        if amount > 0:
+            self.balance += amount
+            print(f"Hai depositato {amount} euro, Il saldo attuale e': {self.balance} euro")
 
-    def update_author(self, new_author):
-        self.author = new_author
-        print(f"Autore aggiornato a '{self.author}'")
-        return self.author
+        else:
+            print("L'importo del deposito deve essere positivo.")
 
-    def __str__(self):
-        return f"Titolo: {self.title}, Autore: {self.author}"
+    def withdraw(self, amount):
+        if amount > 0:
+            if self.balance >= amount:
+                self.balance -= amount
+                print(f"Hai prelevato {amount} euro, Il saldo attuale e':{self.balance} euro")
+
+            else:
+                print("Saldo insufficiente per il prelievo")
+
+        else:
+            print("L'importo del prelievo deve essere positivo.")
 
 
 def main():
-    t = input("Inserisci il titolo: ")
-    a = input("Inserisci l'autore: ")
-    b = Book(str(t), str(a))
-    print(b.getTitle())
-    print(b.getAuthor())
-    print(Book(str(t), str(a)))
-    a = input("Inserisci un nuovo autore: ")
-    print(b.update_author(a))
-    print(b.getAuthor())
-    print(Book(str(t), str(a)))
+    b = BankAccount("Vittorio Tiozzo")
+    print(b.getHolder())
+    print(b.getBalance())
+    print(BankAccount("Vittorio Tiozzo"))
+    b.deposit(100)
+    b.withdraw(50)
+    print(b.getBalance())
+
+    b.withdraw(100)
+    b.withdraw(-3)
+    b.deposit(-5)
+
 
 if __name__ == '__main__':
     main()
