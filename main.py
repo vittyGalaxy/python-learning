104bd7228ce36e898f0a2a061979164c91fe1619
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-07-12 13:47:15 +0200

    summary: exercise 45

--- main.py
+++ main.py
@@ -1,25 +1,40 @@
-class Student:
-    def __init__(self, name):
-        self.name = name
-        self.score = 0
+class Account:
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
 
-    def get_score(self):
-        return self.score
+    def get_username(self):
+        return self.username
 
-    def add_score(self, s):
-        self.score += s
+    def get_email(self):
+        return self.email
+
+    def show_info(self):
+        print(f"Username: {self.username}, Email: {self.email}")
+
+
+class AccountStandard(Account):
+    def show_info(self):
+        super().show_info()
+        print("Tipo di account: Standard")
+
+
+class AdministratorAccount(Account):
+    def __init__(self, username, email, privileges):
+        super().__init__(self, username, email)
+        self.privileges = privileges
+
+    def show_info(self):
+        super().show_info()
+        print(f"Tipo di account: Amministratore, privilegi: {', '.join(self.privileges)}")
 
 
 def main():
-    s = Student("Vittorio")
-    print(s.get_score())
-    s.add_score(6)
-    s.add_score(7)
-    s.add_score(8)
-    s.add_score(9)
-    s.add_score(10)
-    print(s.get_score())
-
+    a_s = AccountStandard("utente_standard", "standard@example.com")
+    a_a = AdministratorAccount("admin", "admin@example.com", ["gestione utenti", "accesso completo"])
+    a_s.show_info()
+    print()
+    a_a.show_info()
 
 
 if __name__ == '__main__':
