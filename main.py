44db186c2091fef51c5068ff11e28fcbb88587ff
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-07-12 20:23:04 +0200

    wip: exercise 51

--- main.py
+++ main.py
@@ -1,58 +1,58 @@
-class MeansOfTransport:
-    def __init__(self, brand, model):
-        self.brand = brand
-        self.model = model
+class Animal:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
 
-    def get_brand(self):
-        return self.brand
+    def get_name(self):
+        return self.name
 
-    def get_model(self):
-        return self.model
-
-    def show_info(self):
-        print(f"Marca: {self.brand}, modello: {self.model}")
-
-
-class Bicycle(MeansOfTransport):
-    def __init__(self, brand, model, type_of_bike):
-        super().__init__(brand, model)
-        self.type_of_bike = type_of_bike
-
-    def get_type_of_bike(self):
-        return self.type_of_bike
+    def get_age(self):
+        return self.age
 
     def show_info(self):
-        super().show_info()
-        print(f"tipo di bici: {self.type_of_bike}")
+        print(f"Marca: {self.name}, modello: {self.age}")
 
 
-class Car(MeansOfTransport):
-    def __init__(self, brand, model, type_of_fuel):
-        super().__init__(brand, model)
-        self.type_of_fuel = type_of_fuel
+class Mammal(Animal):
+    def __init__(self, name, age, type_of_fur):
+        super().__init__(name, age)
+        self.type_of_fur = type_of_fur
 
-    def get_type_of_fuel(self):
-        return self.type_of_fuel
+    def get_type_of_fur(self):
+        return self.type_of_fur
 
     def show_info(self):
         super().show_info()
-        print(f"Tipo di case: {self.type_of_fuel}")
+        print(f"Tipo di pelliccia: {self.type_of_fur}")
+
+
+class Bird(Animal):
+    def __init__(self, name, age, type_of_beak):
+        super().__init__(name, age)
+        self.type_of_beak = type_of_beak
+
+    def get_type_of_beak(self):
+        return self.type_of_beak
+
+    def show_info(self):
+        super().show_info()
+        print(f"Tipo di becco: {self.type_of_beak}")
 
 
 def main():
-    b = Bicycle("Giant", "Defy 3", "Bici da corsa")
-    c = Car("Fiat", "Panda", "Benzina")
-    b.show_info()
+    m = Mammal("Leone", 7, "Lunga e folta")
+    b = Bird("Aquila", 5, "Appuntito")
+    m.show_info()
     print()
-    c.show_info()
+    b.show_info()
 
     print()
-    print(b.get_brand())
-    print(b.get_model())
-    print(b.get_type_of_bike())
-    print(c.get_brand())
-    print(c.get_model())
-    print(c.get_type_of_fuel())
+    print(m.get_name())
+    print(m.get_age())
+    print(m.get_type_of_fur())
+    print(b.get_name())
+    print(b.get_age())
+    print(b.get_type_of_beak())
 
 
 if __name__ == '__main__':
