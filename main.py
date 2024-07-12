1bf0dc161647c752773d7a50129ec6b81f294dbb
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-07-12 17:10:41 +0200

    wip: exercise 46

--- main.py
+++ main.py
@@ -1,40 +1,56 @@
-class Account:
-    def __init__(self, username, email):
-        self.username = username
-        self.email = email
+class HouseholdAppliance:
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
 
-    def get_username(self):
-        return self.username
+    def get_brand(self):
+        return self.brand
 
-    def get_email(self):
-        return self.email
+    def get_model(self):
+        return self.model
 
     def show_info(self):
-        print(f"Username: {self.username}, Email: {self.email}")
+        print(f"Marca: {self.brand}, Modello: {self.model}")
 
 
-class AccountStandard(Account):
-    def show_info(self):
-        super().show_info()
-        print("Tipo di account: Standard")
+class Refrigerator(HouseholdAppliance):
+    def __init__(self, brand, model, capacity):
+        super().__init__(brand, model)
+        self.capacity = capacity
 
-
-class AdministratorAccount(Account):
-    def __init__(self, username, email, privileges):
-        super().__init__(username, email)
-        self.privileges = privileges
+    def get_capacity(self):
+        return self.capacity
 
     def show_info(self):
         super().show_info()
-        print(f"Tipo di account: Amministratore, privilegi: {', '.join(self.privileges)}")
+        print(f"Capacita': {self.capacity} litri")
+
+
+class WashingMachine(HouseholdAppliance):
+    def __init__(self, brand, model, weight):
+        super().__init__(brand, model)
+        self.weight = weight
+
+    def get_weight(self):
+        return self.weight
+
+    def show_info(self):
+        super().show_info()
+        print(f"Carico: {self.weight} kg")
 
 
 def main():
-    a_s = AccountStandard("utente_standard", "standard@example.com")
-    a_a = AdministratorAccount("admin", "admin@example.com", ["gestione utenti", "accesso completo"])
-    a_s.show_info()
+    f = Refrigerator("Samsung", "RT38K5982SL", 384)
+    w = WashingMachine("LG", "F4WV910P2", 10.5)
+    f.show_info()
     print()
-    a_a.show_info()
+    w.show_info()
+    print(f.get_brand())
+    print(f.get_model())
+    print(f.get_capacity())
+    print(w.get_brand())
+    print(w.get_model())
+    print(w.get_weight())
 
 
 if __name__ == '__main__':
