1fec00a893295d9de7cb5b57372912b91a24ac72
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-07-12 12:35:28 +0200

    wip: fixed exercise 38

--- main.py
+++ main.py
@@ -1,30 +1,24 @@
 class Student:
-    def __init__(self, name, surname):
+    def __init__(self, name):
         self.name = name
-        self.surname = surname
-        self.vote = []
+        self.score = 0
 
-    def get_vote(self):
-        return self.vote
+    def get_score(self):
+        return self.score
 
-    def add_vote(self, v):
-        self.vote.append(v)
-
-    def calculate_average(self):
-        average = sum(self.vote) / len(self.get_vote())
-        return average
+    def add_score(self, s):
+        self.score += s
 
 
 def main():
-    s = Student("Vittorio", "Tiozzo")
-    print(s.get_vote())
-    s.add_vote(6)
-    s.add_vote(7)
-    s.add_vote(8)
-    s.add_vote(9)
-    s.add_vote(10)
-    print(s.get_vote())
-    print(s.calculate_average())
+    s = Student("Vittorio")
+    print(s.get_score())
+    s.add_score(6)
+    s.add_score(7)
+    s.add_score(8)
+    s.add_score(9)
+    s.add_score(10)
+    print(s.get_score())
 
 
 
