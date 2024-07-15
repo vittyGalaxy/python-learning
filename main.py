ba73b3669a5c76373f48a3750ff1e32c6c42ccdf
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-07-15 20:04:19 +0200

    summary: exercise 60

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-class ElectricVehicle:
-    def __init__(self, brand, model, battery_capacity, autonomy):
+class PortableDevice:
+    def __init__(self, brand, model, battery_capacity, screen_size, internal_memory):
         self.brand = brand
         self.model = model
         self.battery_capacity = battery_capacity
-        self.autonomy = autonomy
+        self.screen_size = screen_size
+        self.internal_memory = internal_memory
 
     def get_brand(self):
         return self.brand
@@ -14,59 +15,90 @@ class ElectricVehicle:
     def get_battery_capacity(self):
         return self.battery_capacity
 
-    def get_autonomy(self):
-        return self.autonomy
+    def get_screen_size(self):
+        return self.screen_size
+
+    def get_internal_memory(self):
+        return self.internal_memory
 
     def show_info(self):
-        print(f"marca: {self.brand}, modello: {self.model}, capacita batteria: {self.battery_capacity}, autonomia: {self.autonomy}")
+        print(f"marca: {self.brand}, modello: {self.model}, capacita batteria: {self.battery_capacity}, dimensione schermo: {self.screen_size}, memoria interna: {self.internal_memory}")
 
 
-class ElectricCar(ElectricVehicle):
-    def __init__(self, brand, model, battery_capacity, autonomy,number_of_seats):
-        super().__init__(brand, model, battery_capacity, autonomy)
-        self.number_of_seats = number_of_seats
+class Smartphone(PortableDevice):
+    def __init__(self, brand, model, battery_capacity, screen_size, internal_memory, camera_megapixel):
+        super().__init__(brand, model, battery_capacity, screen_size, internal_memory)
+        self.camera_megapixel = camera_megapixel
 
-    def get_number_of_seats(self):
-        return self.number_of_seats
-
-    def show_info(self):
-        super().show_info()
-        print(f"numero posti: {self.number_of_seats}")
-
-
-class ElectricScooter(ElectricVehicle):
-    def __init__(self, brand, model, battery_capacity, autonomy, weight):
-        super().__init__(brand, model, battery_capacity, autonomy)
-        self.weight = weight
-
-    def get_weight(self):
-        return self.weight
+    def get_camera_megapixel(self):
+        return self.camera_megapixel
 
     def show_info(self):
         super().show_info()
-        print(f"peso: {self.weight} kg")
+        print(f"camera megapixel: {self.camera_megapixel}")
+
+
+class Tablet(PortableDevice):
+    def __init__(self, brand, model, battery_capacity, screen_size, internal_memory, pen_support):
+        super().__init__(brand, model, battery_capacity, screen_size, internal_memory)
+        self.pen_support = pen_support
+
+    def get_pen_support(self):
+        return self.pen_support
+
+    def show_info(self):
+        super().show_info()
+        if self.pen_support:
+            self.pen_support = "Si, c'e' la penna"
+
+        else:
+            self.pen_support = "No, non c'e' la penna"
+        print(f"supporto penna: {self.pen_support}")
 
 
 def main():
-    c = ElectricCar("Tesla", "Model 3", 75, 400, 5)
-    s = ElectricScooter("Xiaomi", "Mi Scooter", 0.5, 30, 12)
+    s1 = Smartphone("Apple", "iPhone 14", 3095, 6.1, 128, 12)
+    s2 = Smartphone("Samsung", "Galaxy S23", 3900, 6.8, 256, 50)
+    t1 = Tablet("Microsoft", "Surface Pro 9", 5670, 12.3, 256, True)
+    t2 = Tablet("Samsung", "Galaxy Tab S9", 8000, 11.0, 128, False)
 
-    c.show_info()
+    s1.show_info()
     print()
-    s.show_info()
+    s2.show_info()
+    print()
+    t1.show_info()
+    print()
+    t2.show_info()
     print()
 
-    print(c.get_brand())
-    print(c.get_model())
-    print(c.get_number_of_seats())
-    print(c.get_battery_capacity())
-    print(c.get_autonomy())
 
-    print(s.get_brand())
-    print(s.get_model())
-    print(s.get_battery_capacity())
-    print(s.get_weight())
-    print(s.get_autonomy())
+    print(s1.get_brand())
+    print(s1.get_model())
+    print(s1.get_screen_size())
+    print(s1.get_internal_memory())
+    print(s1.get_camera_megapixel())
+    print(s1.get_battery_capacity())
+
+    print(s2.get_brand())
+    print(s2.get_model())
+    print(s2.get_screen_size())
+    print(s2.get_internal_memory())
+    print(s2.get_camera_megapixel())
+    print(s2.get_battery_capacity())
+
+    print(t1.get_brand())
+    print(t1.get_model())
+    print(t1.get_battery_capacity())
+    print(t1.get_pen_support())
+    print(t1.get_screen_size())
+    print(t1.get_internal_memory())
+
+    print(t2.get_brand())
+    print(t2.get_model())
+    print(t2.get_battery_capacity())
+    print(t2.get_pen_support())
+    print(t2.screen_size)
+    print(t2.internal_memory)
 
 
 if __name__ == '__main__':
