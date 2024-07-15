d3b8d5cc2268652cd6a8e2a7cdb2c948eabf09d3
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-07-15 19:37:49 +0200

    wip: exercise 59

--- main.py
+++ main.py
@@ -1,86 +1,72 @@
-class Building:
-    def __init__(self, address, number_of_floors, construction_year):
-        self.address = address
-        self.number_of_floors = number_of_floors
-        self.costruction_year = construction_year
+class ElectricVehicle:
+    def __init__(self, brand, model, battery_capacity, autonomy):
+        self.brand = brand
+        self.model = model
+        self.battery_capacity = battery_capacity
+        self.autonomy = autonomy
 
-    def get_address(self):
-        return self.address
+    def get_brand(self):
+        return self.brand
 
-    def get_number_of_floors(self):
-        return self.number_of_floors
+    def get_model(self):
+        return self.model
 
-    def get_costruction_year(self):
-        return self.costruction_year
+    def get_battery_capacity(self):
+        return self.battery_capacity
 
-    def show_info(self):
-        print(f"indirizzo: {self.address}, numero di piani: {self.number_of_floors}, anno di costruzione: {self.costruction_year}")
-
-
-class House(Building):
-    def __init__(self, address, number_of_floors, construction_year, number_of_rooms, garden):
-        super().__init__(address, number_of_floors, construction_year)
-        self.number_of_rooms = number_of_rooms
-        self.garden = garden
-
-    def get_number_of_rooms(self):
-        return self.number_of_rooms
-
-    def get_garden(self):
-        return self.garden
+    def get_autonomy(self):
+        return self.autonomy
 
     def show_info(self):
-        super().show_info()
-        if self.garden:
-            self.garden = "C'e' il giardino"
-
-        else:
-            self.garden = "Non c'e' il giardino"
-        print(f"numero camere: {self.number_of_rooms}, giardino: {self.garden}")
+        print(f"marca: {self.brand}, modello: {self.model}, capacita batteria: {self.battery_capacity}, autonomia: {self.autonomy}")
 
 
-class Office(Building):
-    def __init__(self, address, number_of_floors, construction_year, number_offices, elevator):
-        super().__init__(address, number_of_floors, construction_year)
-        self.number_offices = number_offices
-        self.elevator = elevator
+class ElectricCar(ElectricVehicle):
+    def __init__(self, brand, model, battery_capacity, autonomy,number_of_seats):
+        super().__init__(brand, model, battery_capacity, autonomy)
+        self.number_of_seats = number_of_seats
 
-    def get_number_offices(self):
-        return self.number_offices
-
-    def get_elevator(self):
-        return self.elevator
+    def get_number_of_seats(self):
+        return self.number_of_seats
 
     def show_info(self):
         super().show_info()
-        if self.elevator:
-            self.elevator = "C'e' l'ascensore"
+        print(f"numero posti: {self.number_of_seats}")
 
-        else:
-            self.elevator = "Non c'e' l'ascensore"
-        print(f"numero di uffici: {self.number_offices}, l'ascensore: {self.elevator}")
+
+class ElectricScooter(ElectricVehicle):
+    def __init__(self, brand, model, battery_capacity, autonomy, weight):
+        super().__init__(brand, model, battery_capacity, autonomy)
+        self.weight = weight
+
+    def get_weight(self):
+        return self.weight
+
+    def show_info(self):
+        super().show_info()
+        print(f"peso: {self.weight} kg")
 
 
 def main():
-    h = House("Via Roma 1", 2, 1990, 5, True)
-    o = Office("Corso Venezia 15", 5, 2010, 10, True)
+    c = ElectricCar("Tesla", "Model 3", 75, 400, 5)
+    s = ElectricScooter("Xiaomi", "Mi Scooter", 0.5, 30, 12)
 
-    h.show_info()
+    c.show_info()
     print()
-    o.show_info()
+    s.show_info()
     print()
 
-    print(h.get_address())
-    print(h.get_number_of_floors())
-    print(h.get_costruction_year())
-    print(h.get_number_of_rooms())
-    print(h.get_garden())
+    print(c.get_brand())
+    print(c.get_model())
+    print(c.get_number_of_seats())
+    print(c.get_battery_capacity())
+    print(c.get_autonomy())
 
-    print(o.get_address())
-    print(o.get_number_of_floors())
-    print(o.get_costruction_year())
-    print(o.get_number_offices())
-    print(o.get_elevator())
+    print(s.get_brand())
+    print(s.get_model())
+    print(s.get_battery_capacity())
+    print(s.get_weight())
+    print(s.get_autonomy())
 
 
 if __name__ == '__main__':
