f00fbcad72636f046d6d2e748ecca74cef246d6d
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-07-16 10:22:10 +0200

    wip: exercise 61

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-class PortableDevice:
-    def __init__(self, brand, model, battery_capacity, screen_size, internal_memory):
+class Vehicle:
+    def __init__(self, brand, model, year):
         self.brand = brand
         self.model = model
-        self.battery_capacity = battery_capacity
-        self.screen_size = screen_size
-        self.internal_memory = internal_memory
+        self.year = year
 
     def get_brand(self):
         return self.brand
@@ -12,93 +10,86 @@ class PortableDevice:
     def get_model(self):
         return self.model
 
-    def get_battery_capacity(self):
-        return self.battery_capacity
-
-    def get_screen_size(self):
-        return self.screen_size
-
-    def get_internal_memory(self):
-        return self.internal_memory
+    def get_year(self):
+        return self.year
 
     def show_info(self):
-        print(f"marca: {self.brand}, modello: {self.model}, capacita batteria: {self.battery_capacity}, dimensione schermo: {self.screen_size}, memoria interna: {self.internal_memory}")
+        print(f"marca: {self.brand}, modello: {self.model}, anno: {self.year}")
 
 
-class Smartphone(PortableDevice):
-    def __init__(self, brand, model, battery_capacity, screen_size, internal_memory, camera_megapixel):
-        super().__init__(brand, model, battery_capacity, screen_size, internal_memory)
-        self.camera_megapixel = camera_megapixel
+class Car(Vehicle):
+    def __init__(self, brand, model, year, number_of_doors, boot_capacity):
+        super().__init__(brand, model, year)
+        self.number_of_doors = number_of_doors
+        self.boot_capacity = boot_capacity
 
-    def get_camera_megapixel(self):
-        return self.camera_megapixel
+    def get_number_of_doors(self):
+        return self.number_of_doors
+
+    def get_boot_capacity(self):
+        return self.boot_capacity
 
     def show_info(self):
         super().show_info()
-        print(f"camera megapixel: {self.camera_megapixel}")
+        print(f"numero porte: {self.number_of_doors}, capacita' bagaglio: {self.boot_capacity}")
 
 
-class Tablet(PortableDevice):
-    def __init__(self, brand, model, battery_capacity, screen_size, internal_memory, pen_support):
-        super().__init__(brand, model, battery_capacity, screen_size, internal_memory)
-        self.pen_support = pen_support
+class Motorcycle(Vehicle):
+    def __init__(self, brand, model, year, displacement, typee):
+        super().__init__(brand, model, year)
+        self.displacement = displacement
+        self.typee = typee
 
-    def get_pen_support(self):
-        return self.pen_support
+    def get_displacement(self):
+        return self.displacement
+
+    def get_typee(self):
+        return self.typee
 
     def show_info(self):
         super().show_info()
-        if self.pen_support:
-            self.pen_support = "Si, c'e' la penna"
-
-        else:
-            self.pen_support = "No, non c'e' la penna"
-        print(f"supporto penna: {self.pen_support}")
+        print(f"cilindrata: {self.displacement}, tipo: {self.typee}")
 
 
 def main():
-    s1 = Smartphone("Apple", "iPhone 14", 3095, 6.1, 128, 12)
-    s2 = Smartphone("Samsung", "Galaxy S23", 3900, 6.8, 256, 50)
-    t1 = Tablet("Microsoft", "Surface Pro 9", 5670, 12.3, 256, True)
-    t2 = Tablet("Samsung", "Galaxy Tab S9", 8000, 11.0, 128, False)
+    c1 = Car("Fiat", "500", 2019, 3, 185)
+    c2 = Car("Samsung", "Galaxy S23", 3900, 6.8, 256)
+    m1 = Motorcycle("Ducati", "Panigale V4", 2021, 1103, "Sport")
+    m2 = Motorcycle("Harley-Davidson", "Iron 883", 2018, 883, "Cruiser")
 
-    s1.show_info()
+    c1.show_info()
     print()
-    s2.show_info()
+    c2.show_info()
     print()
-    t1.show_info()
+    m1.show_info()
     print()
-    t2.show_info()
+    m2.show_info()
     print()
 
 
-    print(s1.get_brand())
-    print(s1.get_model())
-    print(s1.get_screen_size())
-    print(s1.get_internal_memory())
-    print(s1.get_camera_megapixel())
-    print(s1.get_battery_capacity())
+    print(c1.get_brand())
+    print(c1.get_model())
+    print(c1.get_year())
+    print(c1.get_number_of_doors())
+    print(c1.get_boot_capacity())
 
-    print(s2.get_brand())
-    print(s2.get_model())
-    print(s2.get_screen_size())
-    print(s2.get_internal_memory())
-    print(s2.get_camera_megapixel())
-    print(s2.get_battery_capacity())
+    print(c2.get_brand())
+    print(c2.get_model())
+    print(c2.get_year())
+    print(c2.get_number_of_doors())
+    print(c2.get_boot_capacity())
 
-    print(t1.get_brand())
-    print(t1.get_model())
-    print(t1.get_battery_capacity())
-    print(t1.get_pen_support())
-    print(t1.get_screen_size())
-    print(t1.get_internal_memory())
+    print(m1.get_brand())
+    print(m1.get_model())
+    print(m1.year())
+    print(m1.get_displacement())
+    print(m1.get_typee())
 
-    print(t2.get_brand())
-    print(t2.get_model())
-    print(t2.get_battery_capacity())
-    print(t2.get_pen_support())
-    print(t2.screen_size)
-    print(t2.internal_memory)
+    print(m2.get_brand())
+    print(m2.get_model())
+    print(m2.get_year())
+    print(m2.get_displacement())
+    print(m2.get_typee())
 
 
 if __name__ == '__main__':
