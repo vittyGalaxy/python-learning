cbd2d921a28c1c86eb0cf2b9019dded4b67b7dbf
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-07-17 12:59:58 +0200

    summary: exercise 68

--- main.py
+++ main.py
@@ -1,94 +1,69 @@
-class Product:
-    def __init__(self, name, price, quantity):
+class Plant:
+    def __init__(self, name, height, age):
         self.name = name
-        self.price = price
-        self.quantity = quantity
+        self.height = height
+        self.age = age
 
     def get_name(self):
         return self.name
 
-    def get_price(self):
-        return self.price
+    def get_height(self):
+        return self.height
 
-    def get_quantity(self):
-        return self.quantity
+    def get_age(self):
+        return self.age
 
     def show_info(self):
-        print(f"nome: {self.name}, prezzo: {self.price}, quantita: {self.quantity}")
+        print(f"nome: {self.name}, altezza: {self.height}, eta': {self.age}")
 
 
-class Electronics(Product):
-    def __init__(self, name, price, quantity, warranty, brand):
-        super().__init__(name, price, quantity)
-        self.warranty = warranty
-        self.brand = brand
+class Tree(Plant):
+    def __init__(self, name, height, age, wood_type):
+        super().__init__(name, height, age)
+        self.wood_type = wood_type
 
-    def get_warranty(self):
-        return self.warranty
-
-    def get_brand(self):
-        return self.brand
+    def get_wood_type(self):
+        return self.wood_type
 
     def show_info(self):
         super().show_info()
-        print(f"garanzia: {self.warranty}, marca: {self.brand}")
+        print(f"tipo di legno: {self.wood_type}")
 
 
-class Clothing(Product):
-    def __init__(self, name, price, quantity, size, material):
-        super().__init__(name, price, quantity)
-        self.size = size
-        self.material = material
+class Flower(Plant):
+    def __init__(self, name, height, age, color):
+        super().__init__(name, height, age)
+        self.color = color
 
-    def get_size(self):
-        return self.size
-
-    def get_material(self):
-        return self.material
+    def get_color(self):
+        return self.color
 
     def show_info(self):
         super().show_info()
-        print(f"taglia: {self.size}, materiale: {self.material}")
+        print(f"colore: {self.color}")
 
 
 def main():
-    e1 = Electronics("Smartphone", 599.99, 10, 2, "Samsung")
-    e2 = Electronics("Laptop", 899.99, 5, 3, "Dell")
-    c1 = Clothing("T-shirt", 19.99, 50, "M", "Cotone")
-    c2 = Clothing("Jeans", 49.99, 30, "L", "Denim")
+    t1 = Tree("Quercia", 15, 50, "duro")
+    t2 = Tree("Pino", 20, 30, "morbido")
+    f1 = Flower("Rosa", 0.5, 2, "rosso")
+    f2 = Flower("Tulipano", 0.3, 1, "giallo")
 
-    e1.show_info()
-    print()
-    e2.show_info()
-    print()
-    c1.show_info()
-    print()
-    c2.show_info()
-    print()
+    tree = [t1, t2]
+    for t in tree:
+        t.show_info()
+        print(t.get_name())
+        print(t.get_height())
+        print(t.get_age())
+        print(t.get_wood_type())
 
-    print(e1.get_name())
-    print(e1.get_price())
-    print(e1.get_quantity())
-    print(e1.get_warranty())
-    print(e1.get_brand())
-
-    print(e2.get_name())
-    print(e2.get_price())
-    print(e2.get_quantity())
-    print(e2.get_warranty())
-    print(e2.get_brand())
-
-    print(c1.get_name())
-    print(c1.get_price())
-    print(c1.get_quantity())
-    print(c1.get_size())
-    print(c1.get_material())
-
-    print(c2.get_name())
-    print(c2.get_price())
-    print(c2.get_quantity())
-    print(c2.get_size())
-    print(c2.get_material())
+    flower = [f1, f2]
+    for f in flower:
+        f.show_info()
+        print(f.get_name())
+        print(f.get_height())
+        print(f.get_age())
+        print(f.get_color())
 
 
 if __name__ == '__main__':
