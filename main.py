d710b6bdd57a13ba4d3316f2765710598d38641a
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-07-17 12:17:24 +0200

    wip: exercise 67

--- main.py
+++ main.py
@@ -1,118 +1,94 @@
-from abc import ABC, abstractmethod
-
-
-class TeamMember:
-    def __init__(self, name, surname, age, salary):
+class Product:
+    def __init__(self, name, price, quantity):
         self.name = name
-        self.surname = surname
-        self.age = age
-        self.salary = salary
+        self.price = price
+        self.quantity = quantity
 
     def get_name(self):
         return self.name
 
-    def get_surname(self):
-        return self.surname
+    def get_price(self):
+        return self.price
 
-    def get_age(self):
-        return self.age
-
-    def get_salary(self):
-        return self.salary
+    def get_quantity(self):
+        return self.quantity
 
     def show_info(self):
-        print(f"nome: {self.name}, cognome: {self.surname}, eta': {self.age}, stipendio: {self.salary}")
-
-    def calculate_bonus(self):
-        pass
+        print(f"nome: {self.name}, prezzo: {self.price}, quantita: {self.quantity}")
 
 
-class Developer(TeamMember):
-    def __init__(self, name, surname, age, salary, programming_languages, years_of_experience):
-        super().__init__(name, surname, age, salary)
-        self.programming_languages = programming_languages
-        self.years_of_experience = years_of_experience
+class Electronics(Product):
+    def __init__(self, name, price, quantity, warranty, brand):
+        super().__init__(name, price, quantity)
+        self.warranty = warranty
+        self.brand = brand
 
-    def get_programming_languages(self):
-        return self.programming_languages
+    def get_warranty(self):
+        return self.warranty
 
-    def get_years_of_experience(self):
-        return self.years_of_experience
+    def get_brand(self):
+        return self.brand
 
     def show_info(self):
         super().show_info()
-        print(f"linguaggio programmazione: {self.programming_languages}, anni di esperienza: {self.years_of_experience}")
-
-    def calculate_bonus(self):
-        return self.salary * self.years_of_experience * 0.05
+        print(f"garanzia: {self.warranty}, marca: {self.brand}")
 
 
-class Manager(TeamMember):
-    def __init__(self, name, surname, age, salary, teams_managed, projects_completed):
-        super().__init__(name, surname, age, salary)
-        self.teams_managed = teams_managed
-        self.projects_completed = projects_completed
+class Clothing(Product):
+    def __init__(self, name, price, quantity, size, material):
+        super().__init__(name, price, quantity)
+        self.size = size
+        self.material = material
 
-    def get_teams_managed(self):
-        return self.teams_managed
+    def get_size(self):
+        return self.size
 
-    def get_projects_completed(self):
-        return self.projects_completed
+    def get_material(self):
+        return self.material
 
     def show_info(self):
         super().show_info()
-        print(f"team gestiti: {self.teams_managed}, progetti completati: {self.projects_completed}")
-
-    def calculate_bonus(self):
-        return self.salary * self.projects_completed * 0.1
+        print(f"taglia: {self.size}, materiale: {self.material}")
 
 
 def main():
-    d1 = Developer("Alice", "Verdi", 28, 3500.0, ["Python", "JavaScript"], 5)
-    d2 = Developer("Bob", "Bianchi", 32, 4000.0, ["Java", "C++"], 7)
-    m1 = Manager("Clara", "Rossi", 40, 6000.0, 3, 10)
-    m2 = Manager("David", "Neri", 45, 7000.0, 4, 12)
+    e1 = Electronics("Smartphone", 599.99, 10, 2, "Samsung")
+    e2 = Electronics("Laptop", 899.99, 5, 3, "Dell")
+    c1 = Clothing("T-shirt", 19.99, 50, "M", "Cotone")
+    c2 = Clothing("Jeans", 49.99, 30, "L", "Denim")
 
-    d1.show_info()
-    print(f"Bonus: {d1.calculate_bonus():.2} euro")
+    e1.show_info()
     print()
-    d2.show_info()
-    print(f"Bonus: {d2.calculate_bonus():.2} euro")
+    e2.show_info()
     print()
-    m1.show_info()
-    print(f"Bonus: {m1.calculate_bonus():.2} euro")
+    c1.show_info()
     print()
-    m2.show_info()
-    print(f"Bonus: {m2.calculate_bonus():.2} euro")
+    c2.show_info()
     print()
 
-    print(d1.get_name())
-    print(d1.get_surname())
-    print(d1.get_age())
-    print(d1.get_salary())
-    print(d1.get_programming_languages())
-    print(d1.get_years_of_experience())
+    print(e1.get_name())
+    print(e1.get_price())
+    print(e1.get_quantity())
+    print(e1.get_warranty())
+    print(e1.get_brand())
 
-    print(d2.get_name())
-    print(d2.get_surname())
-    print(d2.get_age())
-    print(d2.get_salary())
-    print(d2.get_programming_languages())
-    print(d2.get_years_of_experience())
+    print(e2.get_name())
+    print(e2.get_price())
+    print(e2.get_quantity())
+    print(e2.get_warranty())
+    print(e2.get_brand())
 
-    print(m1.get_name())
-    print(m1.get_surname())
-    print(m1.get_age())
-    print(m1.get_salary())
-    print(m1.get_teams_managed())
-    print(m1.get_projects_completed())
+    print(c1.get_name())
+    print(c1.get_price())
+    print(c1.get_quantity())
+    print(c1.get_size())
+    print(c1.get_material())
 
-    print(m2.get_name())
-    print(m2.get_surname())
-    print(m2.get_age())
-    print(m2.get_salary())
-    print(m2.get_teams_managed())
-    print(m2.get_projects_completed())
+    print(c2.get_name())
+    print(c2.get_price())
+    print(c2.get_quantity())
+    print(c2.get_size())
+    print(c2.get_material())
 
 
 if __name__ == '__main__':
