5794e61d1e8648fb59a28848d6cc827703f8f32b
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-07-17 19:14:46 +0200

    summary: exercise 72

--- main.py
+++ main.py
@@ -1,109 +1,116 @@
-class Material:
-    def __init__(self, title, authors, year_publication):
-        self.title = title
-        self.authors = authors
-        self.year_publication = year_publication
-
-    def get_title(self):
-        return self.title
-
-    def get_authors(self):
-        return self.authors
-
-    def get_year_publication(self):
-        return self.year_publication
-
-    def show_info(self):
-        print(f"titolo: {self.title}, autori: {self.authors}, anno di pubblicazione: {self.year_publication}")
-
-
-class Book(Material):
-    def __init__(self, title, authors, year_publication, number_of_pages, genre):
-        super().__init__(title, authors, year_publication)
-        self.number_of_pages = number_of_pages
-        self.genre = genre
-
-    def get_number_of_pages(self):
-        return self.number_of_pages
-
-    def get_genre(self):
-        return self.genre
-
-    def show_info(self):
-        super().show_info()
-        print(f"numero di pagine: {self.number_of_pages}, genere: {self.genre}")
-
-
-class Magazine(Material):
-    def __init__(self, title, authors, year_publication, number, publication_month):
-        super().__init__(title, authors, year_publication)
-        self.number = number
-        self.publication_month = publication_month
-
-    def get_publication_month(self):
-        return self.publication_month
-
-    def get_number(self):
-        return self.number
-
-    def show_info(self):
-        super().show_info()
-        print(f"numero: {self.number}, mese di pubblicazione: {self.publication_month}")
-
-
-class DVD(Material):
-    def __init__(self, title, authors, year_publication, duration, genre):
-        super().__init__(title, authors, year_publication)
+class Attraction:
+    def __init__(self, name, minimum_height, capacity, duration):
+        self.name = name
+        self.minimum_height = minimum_height
+        self.capacity = capacity
         self.duration = duration
-        self.genre = genre
+
+    def get_name(self):
+        return self.name
+
+    def get_minimum_height(self):
+        return self.minimum_height
+
+    def get_capacity(self):
+        return self.capacity
 
     def get_duration(self):
         return self.duration
 
-    def get_genre(self):
-        return self.genre
+    def show_info(self):
+        print(f"nome: {self.name}, altezza minima: {self.minimum_height}, capacita': {self.capacity}, durata: {self.duration}")
+
+
+class Carousel(Attraction):
+    def __init__(self, name, minimum_height, capacity, duration, number_of_laps, type_of_ride):
+        super().__init__(name, minimum_height, capacity, duration)
+        self.number_of_laps = number_of_laps
+        self.type_of_ride = type_of_ride
+
+    def get_number_of_laps(self):
+        return self.number_of_laps
+
+    def get_type_of_ride(self):
+        return self.type_of_ride
 
     def show_info(self):
         super().show_info()
-        print(f"durata: {self.duration}, genere: {self.genre}")
+        print(f"numero di giri: {self.number_of_laps}, tipo di giostra: {self.type_of_ride}")
+
+
+class RollerCoaster(Attraction):
+    def __init__(self, name, minimum_height, capacity, duration, max_speed, number_inversions):
+        super().__init__(name, minimum_height, capacity, duration)
+        self.max_speed = max_speed
+        self.number_inversions = number_inversions
+
+    def get_max_speed(self):
+        return self.max_speed
+
+    def get_number_inversions(self):
+        return self.number_inversions
+
+    def show_info(self):
+        super().show_info()
+        print(f"velocita' max: {self.max_speed}, numero di inversioni: {self.number_inversions}")
+
+
+class FerrisWheel(Attraction):
+    def __init__(self, name, minimum_height, capacity, duration, height, number_of_cabins):
+        super().__init__(name, minimum_height, capacity, duration)
+        self.height = height
+        self.number_of_cabins = number_of_cabins
+
+    def get_height(self):
+        return self.height
+
+    def get_number_of_cabins(self):
+        return self.number_of_cabins
+
+    def show_info(self):
+        super().show_info()
+        print(f"altezza: {self.height}, numero di cabine: {self.number_of_cabins}")
 
 
 def main():
-    b1 = Book("Il Signore degli Anelli", ["J.R.R. Tolkien"], 1954, 1216, "Fantasy")
-    b2 = Book("1984", ["George Orwell"], 1949, 328, "Distopia")
+    c1 = Carousel("Carousel Fantasia", 90, 20, 5, 10, "carousel")
+    c2 = Carousel("Disco Magico", 120, 15, 7, 5, "disco")
 
-    m1 = Magazine("National Geographic", ["Vari autori"], 2023, 501, "Luglio")
-    m2 = Magazine("Scientific American", ["Vari autori"], 2023, 1023, "Agosto")
+    r1 = RollerCoaster("Vertigo", 140, 30, 3, 100, 5)
+    r2 = RollerCoaster("Thunderbolt", 160, 40, 4, 120, 8)
 
-    d1 = DVD("Inception", ["Christopher Nolan"], 2010, 148, "Fantascienza")
-    d2 = DVD("Il Padrino", ["Francis Ford Coppola"], 1972, 175, "Dramma")
+    f1 = FerrisWheel("Sky View", 110, 40, 15, 50, 20)
+    f2 = FerrisWheel("Star Ferris", 100, 30, 12, 45, 15)
 
-    book = [b1, b2]
-    for b in book:
-        b.show_info()
-        print(b.get_title())
-        print(b.get_authors())
-        print(b.get_year_publication())
-        print(b.get_number_of_pages())
-        print(b.get_genre())
+    carousel = [c1, c2]
+    for c in carousel:
+        c.show_info()
+        print(c.get_name())
+        print(c.get_minimum_height())
+        print(c.get_capacity())
+        print(c.get_duration())
+        print(c.get_number_of_laps())
+        print(c.get_type_of_ride())
 
-    magazine = [m1, m2]
-    for m in magazine:
-        m.show_info()
-        print(m.get_title())
-        print(m.get_authors())
-        print(m.get_year_publication())
-        print(m.get_number())
-        print(m.get_publication_month())
+    rollerCoaster = [r1, r2]
+    for r in rollerCoaster:
+        r.show_info()
+        print(r.get_name())
+        print(r.get_minimum_height())
+        print(r.get_capacity())
+        print(r.get_duration())
+        print(r.get_max_speed())
+        print(r.get_number_inversions())
 
-    dvd = [d1, d2]
-    for d in dvd:
-        d.show_info()
-        print(d.get_title())
-        print(d.get_authors())
-        print(d.get_year_publication())
-        print(d.get_duration())
-        print(d.get_genre())
+    ferris_wheel = [f1, f2]
+    for f in ferris_wheel:
+        f.show_info()
+        print(f.get_name())
+        print(f.get_minimum_height())
+        print(f.get_capacity())
+        print(f.get_duration())
+        print(f.get_height())
+        print(f.get_number_of_cabins())
 
 
 if __name__ == '__main__':
