cd109fde7241ef706b37733f847e97165788814b
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-07-18 19:10:50 +0200

    summary: exercise 80

--- main.py
+++ main.py
@@ -1,74 +1,74 @@
-class House:
-    def __init__(self, address, surface):
-        self.address = address
-        self.surface = surface
+class Product:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
 
-    def get_address(self):
-        return self.address
+    def get_name(self):
+        return self.name
 
-    def get_surface(self):
-        return self.surface
-
-    def show_info(self):
-        print(f"indirizzo: {self.address}, superficie: {self.surface}")
-
-
-class Apartment(House):
-    def __init__(self, address, surface, floor, number_of_rooms):
-        super().__init__(address, surface)
-        self.floor = floor
-        self.number_of_rooms = number_of_rooms
-
-    def get_floor(self):
-        return self.floor
-
-    def get_number_of_rooms(self):
-        return self.number_of_rooms
+    def get_price(self):
+        return self.price
 
     def show_info(self):
-        super().show_info()
-        print(f"piano: {self.floor}, numero di camere: {self.number_of_rooms}")
+        print(f"nome: {self.name}, prezzo: {self.price}")
 
 
-class Villa(House):
-    def __init__(self, address, surface, garden, number_of_floors):
-        super().__init__(address, surface)
-        self.garden = garden
-        self.number_of_floors = number_of_floors
+class FreshProduct(Product):
+    def __init__(self, name, price, expiration_date, origin):
+        super().__init__(name, price)
+        self.expiration_date = expiration_date
+        self.origin = origin
 
-    def get_garden(self):
-        return self.garden
+    def get_expiration_date(self):
+        return self.expiration_date
 
-    def get_number_of_floors(self):
-        return self.number_of_floors
+    def get_origin(self):
+        return self.origin
 
     def show_info(self):
         super().show_info()
-        print(f"giardino: {self.garden}, numero di piani: {self.number_of_floors}")
+        print(f"data di scadenza: {self.expiration_date}, origine: {self.origin}")
+
+
+class PackagedProduct(Product):
+    def __init__(self, name, price, weight, ingredients):
+        super().__init__(name, price)
+        self.weight = weight
+        self.ingredients = ingredients
+
+    def get_weight(self):
+        return self.weight
+
+    def get_ingredients(self):
+        return self.ingredients
+
+    def show_info(self):
+        super().show_info()
+        print(f"peso: {self.weight}, ingredienti: {self.ingredients}")
 
 
 def main():
-    a1 = Apartment("Via Roma 10", 80, 2, 3)
-    a2 = Apartment("Corso Italia 25", 100, 5, 4)
+    f1 = FreshProduct("Mela", 1.5, "10/07/2024", "Italia")
+    f2 = FreshProduct("Pasta", 0.99, 500, ["Semola di grano duro"])
 
-    v1 = Villa("Via delle Rose 15", 250, True, 2)
-    v2 = Villa("Via dei Gelsomini 20", 300, False, 3)
+    p1 = PackagedProduct("Pesce", 10.99, "15/07/2024", "Mare del Nord")
+    p2 = PackagedProduct("Biscotti", 2.49, 300, ["Farina", "Zucchero", "Burro"])
 
-    apartment = [a1, a2]
-    for a in apartment:
-        a.show_info()
-        print(a.get_address())
-        print(a.get_surface())
-        print(a.get_floor())
-        print(a.get_number_of_rooms())
+    freshProduct = [f1, f2]
+    for f in freshProduct:
+        f.show_info()
+        print(f.get_name())
+        print(f.get_price())
+        print(f.get_expiration_date())
+        print(f.get_origin())
 
-    villa = [v1, v2]
-    for v in villa:
-        v.show_info()
-        print(v.get_address())
-        print(v.get_surface())
-        print(v.get_garden())
-        print(v.get_number_of_floors())
+    packaged_product = [p1, p2]
+    for p in packaged_product:
+        p.show_info()
+        print(p.get_name())
+        print(p.get_price())
+        print(p.get_weight())
+        print(p.get_ingredients())
 
 
 if __name__ == '__main__':
