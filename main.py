f6354c15eec04b333e296a12f05823169b3baad8
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-07-19 10:39:50 +0200

    wip: exercise 81

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 class Product:
-    def __init__(self, name, price):
+    def __init__(self, name, price, amount):
         self.name = name
         self.price = price
+        self.amount = amount
 
     def get_name(self):
         return self.name
@@ -9,66 +10,109 @@ class Product:
     def get_price(self):
         return self.price
 
-    def show_info(self):
-        print(f"nome: {self.name}, prezzo: {self.price}")
-
-
-class FreshProduct(Product):
-    def __init__(self, name, price, expiration_date, origin):
-        super().__init__(name, price)
-        self.expiration_date = expiration_date
-        self.origin = origin
-
-    def get_expiration_date(self):
-        return self.expiration_date
-
-    def get_origin(self):
-        return self.origin
+    def get_amount(self):
+        return self.amount
 
     def show_info(self):
-        super().show_info()
-        print(f"data di scadenza: {self.expiration_date}, origine: {self.origin}")
+        print(f"nome: {self.name}, prezzo: {self.price}, quantita': {self.amount}")
+
+    def calculate_counted_price(self, discount):
+        discounted_price = self.price * (1 - discount / 100)
+        return round(discounted_price, 2)
 
 
-class PackagedProduct(Product):
-    def __init__(self, name, price, weight, ingredients):
-        super().__init__(name, price)
-        self.weight = weight
-        self.ingredients = ingredients
+class Computer(Product):
+    def __init__(self, name, price, amount, processor, ram):
+        super().__init__(name, price, amount)
+        self.processor = processor
+        self.ram = ram
 
-    def get_weight(self):
-        return self.weight
+    def get_ram(self):
+        return self.ram
 
-    def get_ingredients(self):
-        return self.ingredients
+    def get_processor(self):
+        return self.processor
 
     def show_info(self):
         super().show_info()
-        print(f"peso: {self.weight}, ingredienti: {self.ingredients}")
+        print(f"ram: {self.ram}, processore: {self.processor}")
+
+
+class Telephone(Product):
+    def __init__(self, name, price, amount, brand, model):
+        super().__init__(name, price, amount)
+        self.brand = brand
+        self.model = model
+
+    def get_brand(self):
+        return self.brand
+
+    def get_model(self):
+        return self.model
+
+    def show_info(self):
+        super().show_info()
+        print(f"marca: {self.brand}, modello: {self.model}")
+
+
+class Television(Product):
+    def __init__(self, name, price, amount, size, resolution):
+        super().__init__(name, price, amount)
+        self.size = size
+        self.resolution = resolution
+
+    def get_size(self):
+        return self.size
+
+    def get_resolution(self):
+        return self.resolution
+
+    def show_info(self):
+        super().show_info()
+        print(f"dimensioni: {self.size}, risoluzione: {self.resolution}")
 
 
 def main():
-    f1 = FreshProduct("Mela", 1.5, "10/07/2024", "Italia")
-    f2 = FreshProduct("Pasta", 0.99, 500, ["Semola di grano duro"])
+    c1 = Computer("Laptop HP", 1200.0, 10, "Intel i7", 16)
+    c2 = Computer("MacBook Pro", 2500.0, 5, "Apple M1", 16)
 
-    p1 = PackagedProduct("Pesce", 10.99, "15/07/2024", "Mare del Nord")
-    p2 = PackagedProduct("Biscotti", 2.49, 300, ["Farina", "Zucchero", "Burro"])
+    t1 = Telephone("iPhone 13", 999.0, 15, "Apple", "13")
+    t2 = Telephone("Samsung Galaxy S21", 799.0, 20, "Samsung", "Galaxy S21")
 
-    freshProduct = [f1, f2]
-    for f in freshProduct:
-        f.show_info()
-        print(f.get_name())
-        print(f.get_price())
-        print(f.get_expiration_date())
-        print(f.get_origin())
+    t3 = Television("LG OLED55", 1800.0, 7, 55, "4K")
+    t4 = Television("Sony Bravia", 1500.0, 8, 65, "4K")
 
-    packaged_product = [p1, p2]
-    for p in packaged_product:
-        p.show_info()
-        print(p.get_name())
-        print(p.get_price())
-        print(p.get_weight())
-        print(p.get_ingredients())
+    discount = 10
+
+    computer = [c1, c2]
+    for c in computer:
+        c.show_info()
+        print(c.get_name())
+        print(c.get_price())
+        print(c.get_amount())
+        print(c.get_ram())
+        print(c.get_processor())
+        print(c.calculate_counted_price(discount))
+
+    telephone = [t1, t2]
+    for t in telephone:
+        t.show_info()
+        print(t.get_name())
+        print(t.get_price())
+        print(t.get_amount())
+        print(t.get_brand())
+        print(t.get_model())
+        print(t.calculate_counted_price(discount))
+
+    television = [t3, t4]
+    for t in television:
+        t.show_info()
+        print(t.get_name())
+        print(t.get_price())
+        print(t.get_amount())
+        print(t.get_size())
+        print(t.get_resolution())
+        print(t.calculate_counted_price(discount))
 
 
 if __name__ == '__main__':
