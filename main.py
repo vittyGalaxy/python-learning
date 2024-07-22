18d297055d8900c3ac2c993514c7aa2937cc41db
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-07-22 13:29:53 +0200

    summary: learning GUI button

--- main.py
+++ main.py
@@ -1,21 +1,19 @@
 import tkinter
 
+root = tkinter.Tk()
+
+
+def click_here():
+    write = tkinter.Label(root, text="Pulsante premuto!")
+    write.pack()
+
 
 def main():
-    root = tkinter.Tk()
 
-    my_label = tkinter.Label(root, text="Ciao sono Vittorio")
-    my_label2 = tkinter.Label(root, text="Ti presento GUI")
-    my_label3 = tkinter.Label(root, text="-----------")
+   my_button = tkinter.Button(root, text="Clicca qui!", command=click_here, padx=30)
+   my_button.pack()
 
-    my_label.grid(row=0, column=0)
-    my_label2.grid(row=1, column=0)
-    my_label3.grid(row=1, column=1)
-
-    my_label2.grid(row=1, column=5)
-    my_label3.grid(row=1, column=1)
-
-    root.mainloop()
+   root.mainloop()
 
 
 if __name__ == '__main__':
