271a10bd8e0fc71a230966ad59563b4d74871a8c
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-07-22 20:58:46 +0200

    summary: complete calculator

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ root = tkinter.Tk()
 root.title("La mia calcolatrice")
 
 enter_text = tkinter.Entry(root, width=35, borderwidth=5)
-enter_text.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
+enter_text.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
 
 
 def click_btn(number):
@@ -20,6 +20,35 @@ def btn_canc():
 def btn_sum():
     first_number = enter_text.get()
     global f_num
+    global op_mat
+    op_mat = "somma"
+    f_num = int(first_number)
+    enter_text.delete(0, "end")
+
+
+def btn_sub():
+    first_number = enter_text.get()
+    global f_num
+    global op_mat
+    op_mat = "sottrazione"
+    f_num = int(first_number)
+    enter_text.delete(0, "end")
+
+
+def btn_mul():
+    first_number = enter_text.get()
+    global f_num
+    global op_mat
+    op_mat = "moltiplicazione"
+    f_num = int(first_number)
+    enter_text.delete(0, "end")
+
+
+def btn_div():
+    first_number = enter_text.get()
+    global f_num
+    global op_mat
+    op_mat = "divisione"
     f_num = int(first_number)
     enter_text.delete(0, "end")
 
@@ -27,7 +56,17 @@ def btn_sum():
 def btn_equal():
     second_number = enter_text.get()
     enter_text.delete(0, "end")
-    enter_text.insert(0, f_num + int(second_number))
+    if op_mat == "somma":
+        enter_text.insert(0, f_num + int(second_number))
+
+    elif op_mat == "sottrazione":
+        enter_text.insert(0, f_num - int(second_number))
+
+    elif op_mat == "moltiplicazione":
+        enter_text.insert(0, f_num * int(second_number))
+
+    elif op_mat == "divisione":
+        enter_text.insert(0, f_num / int(second_number))
 
 
 btn_1 = tkinter.Button(root, text="1", padx=40, pady=20, command=lambda: click_btn(1))
@@ -40,9 +79,12 @@ btn_7 = tkinter.Button(root, text="7", padx=40, pady=20, command=lambda: click_b
 btn_8 = tkinter.Button(root, text="8", padx=40, pady=20, command=lambda: click_btn(8))
 btn_9 = tkinter.Button(root, text="9", padx=40, pady=20, command=lambda: click_btn(9))
 btn_0 = tkinter.Button(root, text="0", padx=40, pady=20, command=lambda: click_btn(0))
-btn_sum = tkinter.Button(root, text="+", padx=39, pady=20, command=btn_sum)
+btn_summ = tkinter.Button(root, text="+", padx=28, pady=20, command=btn_sum)
+btn_subb = tkinter.Button(root, text="-", padx=31, pady=20, command=btn_sub)
+btn_mull = tkinter.Button(root, text="*", padx=30, pady=20, command=btn_mul)
+btn_divv = tkinter.Button(root, text="/", padx=31, pady=20, command=btn_div)
 btn_equal = tkinter.Button(root, text="=", padx=39, pady=20, command=btn_equal)
-btn_cancel = tkinter.Button(root, text="Cancella", padx=199, pady=20, command= btn_canc)
+btn_cancel = tkinter.Button(root, text="Canc", padx=28, pady=20, command= btn_canc)
 
 btn_1.grid(row=3, column=0)
 btn_2.grid(row=3, column=1)
@@ -56,11 +98,14 @@ btn_7.grid(row=1, column=0)
 btn_8.grid(row=1, column=1)
 btn_9.grid(row=1, column=2)
 
-btn_0.grid(row=4, column=0)
+btn_0.grid(row=4, column=1)
 
-btn_sum.grid(row=4, column=1)
+btn_summ.grid(row=1, column=4)
+btn_subb.grid(row=2, column=4)
+btn_mull.grid(row=3, column=4)
+btn_divv.grid(row=4, column=4)
 btn_equal.grid(row=4, column=2)
-btn_cancel.grid(row=5, column=0, columnspan=3)
+btn_cancel.grid(row=4, column=0)
 
 
 def main():
