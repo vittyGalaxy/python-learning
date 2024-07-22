44b4915838073a102c91ed6608779759f02a5587
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-07-22 10:57:32 +0200

    wip: learning grid

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import tkinter
 
+
 def main():
     root = tkinter.Tk()
 
     my_label = tkinter.Label(root, text="Ciao sono Vittorio")
+    my_label2 = tkinter.Label(root, text="Ti presento GUI")
+    my_label3 = tkinter.Label(root, text="-----------")
 
-    my_label.pack()
+    my_label.grid(row=0, column=0)
+    my_label2.grid(row=1, column=0)
+    my_label3.grid(row=1, column=1)
+
+    my_label2.grid(row=1, column=5)
+    my_label3.grid(row=1, column=1)
 
     root.mainloop()
 
