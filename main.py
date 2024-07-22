78f9046affcb81069c3ffff1922045334fce73ae
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-07-22 16:46:20 +0200

    wip: learning GUI input field

--- main.py
+++ main.py
@@ -3,14 +3,19 @@ import tkinter
 root = tkinter.Tk()
 
 
+enter_text = tkinter.Entry(root, width=60, bg="black", fg="white")
+enter_text.pack()
+
+
 def click_here():
-    write = tkinter.Label(root, text="Pulsante premuto!")
+    welcome = "Ciao " + enter_text.get()
+    write = tkinter.Label(root, text=welcome)
     write.pack()
 
 
 def main():
 
-   my_button = tkinter.Button(root, text="Clicca qui!", command=click_here, padx=30)
+   my_button = tkinter.Button(root, text="Inserisci il tuo nome", command=click_here, padx=30)
    my_button.pack()
 
    root.mainloop()
