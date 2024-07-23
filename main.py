6dcd20aebdf2f4b091a8e5a008528651639db873
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-07-23 17:16:19 +0200

    wip: exercise 84

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import tkinter
-import random
+
+count = 0
 
 
-def change_color():
-    colors = ["red", "green", "yellow", "purple", "orange"]
-    root.config(bg=random.choice(colors))
+def counter():
+    global count
+    count += 1
+    print(count)
 
 
 root = tkinter.Tk()
@@ -14,9 +16,9 @@ root.geometry("300x200")
 enter_text = tkinter.Entry(root, width=60, bg="black", fg="white")
 enter_text.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
 
-btn_color = tkinter.Button(root, text="change color", command=change_color)
+btn_count = tkinter.Button(root, text="click me", command=counter)
 
-btn_color.grid(row=1, column=1)
+btn_count.grid(row=1, column=1)
 
 
 def main():
