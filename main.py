649fa00515ab7eb33ae79f8c3866f9b114dbce69
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-07-23 16:27:19 +0200

    wip: exercise GUI

--- main.py
+++ main.py
@@ -1,40 +1,22 @@
 import tkinter
+import random
+
+
+def change_color():
+    colors = ["red", "green", "yellow", "purple", "orange"]
+    root.config(bg=random.choice(colors))
+
 
 root = tkinter.Tk()
-root.title("Salva nomi")
+root.title("cambio colore")
+root.geometry("300x200")
 
 enter_text = tkinter.Entry(root, width=60, bg="black", fg="white")
 enter_text.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
 
-save = []
+btn_color = tkinter.Button(root, text="change color", command=change_color)
 
-
-def btn_save():
-    t = enter_text.get()
-    save.append(t)
-    f = open("file.txt", "w")
-    f.write(str(save))
-    f.close()
-    s = "salvato il nome " + t
-    write = tkinter.Label(root, text=s)
-
-
-def btn_load():
-    load = []
-    f = open("file.txt", "r")
-    for l in f:
-        load.append(l)
-    f.close()
-
-    print(load)
-
-
-btn_s = tkinter.Button(root, text="save", padx=39, pady=20, command=btn_save)
-btn_l = tkinter.Button(root, text="load", padx=39, pady=20, command=btn_load)
-
-
-btn_s.grid(row=1, column=0)
-btn_l.grid(row=1, column=1)
+btn_color.grid(row=1, column=1)
 
 
 def main():
