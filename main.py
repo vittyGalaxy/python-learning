2eebd0dc7439aa687faf66cc3ea3b5766d5e8dc5
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-07-23 19:01:24 +0200

    summary: exercise 85

--- main.py
+++ main.py
@@ -1,24 +1,56 @@
 import tkinter
 
-count = 0
+not_domestic_animal = []
+domestic_animal = []
 
 
-def counter():
-    global count
-    count += 1
-    print(count)
+def button_save():
+    t = enter_text.get()
+    if (t == "cat") or (t == "dog") or (t == "rabbit") or (t == "hamster") or (t == "turtle"):
+        domestic_animal.append(t)
+
+    else:
+        not_domestic_animal.append(t)
+
+    f = open("file_copia.txt", "w")
+    f.write(str(domestic_animal))
+    f.close()
+
+    f = open("file.txt", "w")
+    f.write(str(not_domestic_animal))
+    f.close()
+
+
+def button_load():
+    load_domestic = []
+    load_not_domestic = []
+    print("Gli animali domestici sono: ")
+    f = open("file_copia.txt", "r")
+    for l in f:
+        load_domestic.append(l)
+    f.close()
+
+    f = open("file.txt", "r")
+    for l in f:
+        load_not_domestic.append(l)
+    f.close()
+    print(load_domestic)
+    print("Gli animali non domestici: ")
+    print(load_not_domestic)
 
 
 root = tkinter.Tk()
-root.title("cambio colore")
+root.title("Riconoscimento")
 root.geometry("300x200")
 
 enter_text = tkinter.Entry(root, width=60, bg="black", fg="white")
 enter_text.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
 
-btn_count = tkinter.Button(root, text="click me", command=counter)
+btn_save = tkinter.Button(root, text="save", command=button_save)
+btn_load = tkinter.Button(root, text="load", command=button_load)
 
-btn_count.grid(row=1, column=1)
+btn_save.grid(row=1, column=1)
+btn_load.grid(row=1, column=2)
 
 
 def main():
