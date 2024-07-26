512fb51bbe5f443db34058baf27a0943cc561e6e
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-07-26 19:30:41 +0200

    summary: exercise numpy 3

--- main.py
+++ main.py
@@ -2,22 +2,30 @@ import numpy as np
 
 
 def main():
-    array_1d = np.arange(10)
-    print("Array 1D", array_1d)
+    array_1d = np.arange(1, 16)
+    print("Array 1D: ", array_1d)
 
-    summ = np.sum(array_1d)
-    maxx = np.max(array_1d)
-    minn = np.min(array_1d)
-    average = np.mean(array_1d)
-    print("La somma e': ", summ)
-    print("Il massimo e': ", maxx)
-    print("Il minore e': ", minn)
-    print("La media e': ", average)
+    array_2d = array_1d.reshape(3, 5)
+    print("Array 2D:\n", array_2d)
 
-    mask = array_1d > 5
-    major_elements_5 = array_1d[mask]
-    print("Maschera booleana per elementi > 5:\n", mask)
-    print("Elementi maggiori di 5: ", major_elements_5)
+    second_row = array_2d[1, :]
+    print("Seconda riga", second_row)
+
+    third_column = array_2d[:, 2]
+    print("Terza colonna:", third_column)
+
+    mask = array_2d > 10
+    major_elements_10 = array_2d[mask]
+    print("Maschera booleana per elementi > 10:\n", mask)
+    print("Elementi maggiori di 10: ", major_elements_10)
+
+    sum_rows = np.sum(array_2d, axis=1)
+    sum_columns = np.sum(array_2d, axis=0)
+    print("Somma degli elementi di ciascuna riga: ", sum_rows)
+    print("Somma degli elementi di ciascuna colonna: ", sum_columns)
+
+    rows_sum_major_40 = array_2d[sum_rows > 40, :]
+    print("Righe con somma degli elementi maggiore di 40:\n", rows_sum_major_40)
 
 
 if __name__ == '__main__':
