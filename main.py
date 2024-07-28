f3b79c84aab07f900f4f9d90b2f2cff16d0beeb6
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-07-28 10:49:46 +0200

    wip: exercise 87

--- main.py
+++ main.py
@@ -2,30 +2,18 @@ import numpy as np
 
 
 def main():
-    array_1d = np.arange(1, 16)
-    print("Array 1D: ", array_1d)
+    temperature = np.random.uniform(low=0, high=40, size=365)
 
-    array_2d = array_1d.reshape(3, 5)
-    print("Array 2D:\n", array_2d)
+    average_temperature = np.mean(temperature)
+    max_temperature = np.max(temperature)
+    min_temperature = np.min(temperature)
+    print(f"Temperatura media: {average_temperature:.2f} gradi")
+    print(f"Temperatura massima: {max_temperature:.2f} gradi")
+    print(f"Temperatura minima: {min_temperature:.2f} gradi")
 
-    second_row = array_2d[1, :]
-    print("Seconda riga", second_row)
-
-    third_column = array_2d[:, 2]
-    print("Terza colonna:", third_column)
-
-    mask = array_2d > 10
-    major_elements_10 = array_2d[mask]
-    print("Maschera booleana per elementi > 10:\n", mask)
-    print("Elementi maggiori di 10: ", major_elements_10)
-
-    sum_rows = np.sum(array_2d, axis=1)
-    sum_columns = np.sum(array_2d, axis=0)
-    print("Somma degli elementi di ciascuna riga: ", sum_rows)
-    print("Somma degli elementi di ciascuna colonna: ", sum_columns)
-
-    rows_sum_major_40 = array_2d[sum_rows > 40, :]
-    print("Righe con somma degli elementi maggiore di 40:\n", rows_sum_major_40)
+    hot_days = np.where(temperature > 30)[0]
+    number_of_hot_days = len(hot_days)
+    print(f"Giorni con temperatura > 30 gradi: {number_of_hot_days}")
 
 
 if __name__ == '__main__':
