c54627dad7010c93771713e15cdce36c368b6138
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-08-16 13:23:04 +0200

    wip: exercise 95

--- main.py
+++ main.py
@@ -1,14 +1,41 @@
+class Counter:
+    def __init__(self):
+        self.c = 0
+
+    # getter
+    def get_counter(self):
+        return self.c
+
+    # setter
+    def set_counter(self, number):
+        self.c = number
+
+    def increment(self):
+        self.c += 1
+
+    def decrement(self):
+        self.c -= 1
+
+
 def main():
-    n = 10
-
-    counter = 0
-    for i in range(0, int(n)):
-        a = input("Inserisci numero: ")
-
-        if int(a) == 9:
-            counter += 1
-
-    print("I numeri 9 sono: ", str(counter))
+    counter = Counter()
+    print(counter.get_counter())
+    counter.increment()
+    print(counter.get_counter())
+    counter.decrement()
+    print(counter.get_counter())
+    counter.set_counter(3)
+    print(counter.get_counter())
+    counter.increment()
+    print(counter.get_counter())
+    counter.decrement()
+    print(counter.get_counter())
+    counter.decrement()
+    print(counter.get_counter())
+    counter.decrement()
+    print(counter.get_counter())
+    counter.decrement()
+    print(counter.get_counter())
 
 
 if __name__ == '__main__':
