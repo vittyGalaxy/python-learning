9fd36769affbc25adfb18de7d954fbbf8498ae07
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-08-17 13:52:06 +0200

    summary: exercise 100

--- main.py
+++ main.py
@@ -3,15 +3,36 @@ import numpy as np
 
 
 def main():
-    matrix = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-    print(matrix)
-    print(matrix.sum())
-    print(np.sum(matrix, axis=0))
-    print(np.prod(matrix, axis=1))
-    max_value = np.max(matrix)
-    max_index = np.unravel_index(np.argmax(matrix), matrix.shape)
-    print(max_value)
-    print(max_index)
+    arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+    print(arr)
+    even_numbers = []
+
+    arr2 = []
+    for i in arr:
+        if i % 2 == 0:
+            even_numbers.append(i)
+            arr2.append(i)
+
+        else:
+            arr2.append(-1)
+
+    print(even_numbers)
+    print(arr2)
+
+    arr3 = []
+    for i in arr:
+        if 5 < i < 15:
+            arr3.append(i)
+
+    print(arr3)
+
+    arr_copy = []
+    for i in arr:
+        arr_copy.append(i * 2)
+
+    print(arr_copy)
+
+
 
 if __name__ == '__main__':
     main()
