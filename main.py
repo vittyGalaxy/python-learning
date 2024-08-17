8d428af03b0eaff5d83754cbd2240a7b23c655d9
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-08-17 13:03:59 +0200

    wip: exercise 99

--- main.py
+++ main.py
@@ -3,23 +3,15 @@ import numpy as np
 
 
 def main():
-    a = np.arange(0, 10)
-    print(a)
-    b = np.arange(10, 20)
-    print(b)
-    c = a + b
-    print(c)
-    print(c.mean())
-    d = c * 2
-    print(d)
-
-    e = []
-    for i in d:
-        if i > 20:
-            e.append(i)
-
-    print(e)
-
+    matrix = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    print(matrix)
+    print(matrix.sum())
+    print(np.sum(matrix, axis=0))
+    print(np.prod(matrix, axis=1))
+    max_value = np.max(matrix)
+    max_index = np.unravel_index(np.argmax(matrix), matrix.shape)
+    print(max_value)
+    print(max_index)
 
 if __name__ == '__main__':
     main()
